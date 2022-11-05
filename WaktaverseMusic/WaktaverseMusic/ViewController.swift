d60630e84181a39cb18bb9f269e06685aa7d36e7
yongbeomkwak <yongbeomkwak@gmail.com>
2022-11-05 15:26:46 +0900

    [CHORE] 프로젝트 초기 셋팅

---
+++ WaktaverseMusic/WaktaverseMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WaktaverseMusic
+//
+//  Created by yongbeomkwak on 2022/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
