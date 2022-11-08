482b398adefff8c17cfd2e23be4696a9256b8439
KyungJoon <dragoa90@gmail.com>
2022-11-08 18:21:26 +0900

    ♻️ Framework 추가, Pods 이슈로 compatible 14 -> 13으로 변경

---
+++ WaktaverseMusic/WaktaverseMusicFramework/WaktaverseMusicFramework/WaktaverseMusicFramework.h
@@ -0,0 +1,18 @@
+//
+//  WaktaverseMusicFramework.h
+//  WaktaverseMusicFramework
+//
+//  Created by Fo co on 2022/11/08.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WaktaverseMusicFramework.
+FOUNDATION_EXPORT double WaktaverseMusicFrameworkVersionNumber;
+
+//! Project version string for WaktaverseMusicFramework.
+FOUNDATION_EXPORT const unsigned char WaktaverseMusicFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaktaverseMusicFramework/PublicHeader.h>
+
+
