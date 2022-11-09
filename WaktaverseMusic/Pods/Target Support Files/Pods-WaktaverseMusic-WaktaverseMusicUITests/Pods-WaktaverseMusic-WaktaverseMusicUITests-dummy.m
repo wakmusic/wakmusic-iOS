984510312c931ae758fae298c999ea360e5ed387
Hugh.Park <31872539+CoCoE1203@users.noreply.github.com>
2022-11-09 10:18:50 +0900

    [CHORE] Pods 삭제
    gitignore 적용을 위함

--- WaktaverseMusic/Pods/Target Support Files/Pods-WaktaverseMusic-WaktaverseMusicUITests/Pods-WaktaverseMusic-WaktaverseMusicUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WaktaverseMusic_WaktaverseMusicUITests : NSObject
-@end
-@implementation PodsDummy_Pods_WaktaverseMusic_WaktaverseMusicUITests
-@end
--- WaktaverseMusic/Pods/Target Support Files/Pods-WaktaverseMusic/Pods-WaktaverseMusic-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WaktaverseMusic : NSObject
-@end
-@implementation PodsDummy_Pods_WaktaverseMusic
-@end
