803b3bb279decab25b9249d22bc67d976c8c1b1d
댕댕 <dragoa90@gmail.com>
2022-11-14 16:59:52 +0900

    [CHORE] initView 적용
    RootViewController init 구성 적용

--- WaktaverseMusic/WaktaverseMusic/Info/WaktaverseMusic-Bridging-Header.h
+++ WaktaverseMusic/WaktaverseMusic/Info/WaktaverseMusic-Bridging-Header.h
@@ -5,6 +5,5 @@
 #ifndef WaktaverseMusic-Bridging-Header_h
 #define WaktaverseMusic-Bridging-Header_h
 
-#define aGate ((AppDelegate*)[[UIApplication sharedApplication] delegate])
 
 #endif /* WaktaverseMusic-Bridging-Header_h */
--- WaktaverseMusic/WaktaverseMusic/Screen/Root/RootViewController.swift
+++ WaktaverseMusic/WaktaverseMusic/Screen/Root/RootViewController.swift
@@ -16,6 +16,7 @@ class RootViewController: LGSideMenuController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        initView()
     }
 
     /*
