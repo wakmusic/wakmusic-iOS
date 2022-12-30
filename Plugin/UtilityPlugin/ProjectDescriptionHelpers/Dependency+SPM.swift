ffbe235a014a256fbcf829acbd3b69c12430facb
김대희 <0824dh@naver.com>
2022-12-30 20:21:35 +0900

    🔧 :: swiftPackageManager SPM 모듈 설정

--- Plugin/UtilityPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ Plugin/UtilityPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -5,6 +5,21 @@ public extension TargetDependency {
 }
 
 public extension TargetDependency.SPM {
+    static let Moya = TargetDependency.external(name: "Moya")
+    static let RxMoya = TargetDependency.external(name: "RxMoya")
+    static let YouTubePlayerKit = TargetDependency.external(name: "YouTubePlayerKit")
+    static let PanModal = TargetDependency.external(name: "PanModal")
+    static let RxSwift = TargetDependency.external(name: "RxSwift")
+    static let RxCocoa = TargetDependency.external(name: "RxCocoa")
+    static let ReactorKit = TargetDependency.external(name: "ReactorKit")
+    static let Kingfisher = TargetDependency.external(name: "Kingfisher")
+    static let Then = TargetDependency.external(name: "Then")
+    static let SnapKit = TargetDependency.external(name: "SnapKit")
+    static let ReachabilitySwift = TargetDependency.external(name: "ReachabilitySwift")
+    static let Lottie = TargetDependency.external(name: "Lottie")
+    static let Firebase = TargetDependency.external(name: "Firebase")
+    static let PinLayout = TargetDependency.external(name: "PinLayout")
+    static let FlexLayout = TargetDependency.external(name: "FlexLayout")
     static let Quick = TargetDependency.external(name: "Quick")
     static let Nimble = TargetDependency.external(name: "Nimble")
     static let Needle = TargetDependency.external(name: "NeedleFoundation")
--- Projects/Features/CommonFeature/Project.swift
+++ Projects/Features/CommonFeature/Project.swift
@@ -8,6 +8,6 @@ let project = Project.makeModule(
         .Project.Service.Domain,
         .Project.Module.Utility,
         .Project.Module.FeatureThirdPartyLib,
-        .Project.UserInterfaces.DesignSystem
+        .Project.UserInterfaces.DesignSystem,
     ]
 )
