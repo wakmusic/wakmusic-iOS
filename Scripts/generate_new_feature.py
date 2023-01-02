e7f2b523ff13b84a89fa150e6aa2f4fafd1f363f
김대희 <0824dh@naver.com>
2023-01-02 10:28:39 +0900

    🔧 ::  Python 파일 수정

--- Scripts/generate_new_feature.py
+++ Scripts/generate_new_feature.py
@@ -30,15 +30,14 @@ def make_dirs(paths):
 def make_project_file(feature_name, file_path, has_demo=False, dependencies=[]):
     project_path = file_path + '/Project.swift'
     file_name = file_path.split('/')[-1]
-    file_content = f"""
-import ProjectDescription
+    file_content = f"""import ProjectDescription
 import ProjectDescriptionHelpers
 
 let project = Project.makeModule(
     name: "{feature_name}Feature",
     product: .staticFramework,
     dependencies: [
-        .Project.Features.CommonFeature"""
+        .Project.Features.BaseFeature"""
     file_content += ",\n        ".join(dependencies)
     file_content += "\n    ]" 
     file_content += ",\n    hasDemo: true" if has_demo else ""
@@ -131,11 +130,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 '''
     write_code_in_file(app_delegate_path, app_delegate)
 
+def write_created_feature_on_dependency_project(feature_name):
+    file_path = f'{root_path}/Plugin/UtilityPlugin/ProjectDescriptionHelpers/Dependency+Project.swift'
+    read_file = read_file_at(file_path)
+    updated_file = update_file_at(read_file, feature_name)
+    write_file_at(file_path, updated_file)
 
+def read_file_at(file_path):
+    with open(file_path, 'r') as file:
+        return file.readlines()
+
+def update_file_at(dependency_file, feature_name):
+    insert_line = 0
+    dependency_file_len = len(dependency_file)
+    for index, elem in enumerate(dependency_file):
+        if "public extension TargetDependency.Project.Features" in elem:
+            insert_line = index + 1
+            break
+        
+    append_code = f'    static let {feature_name}Feature = TargetDependency.feature(name: "{feature_name}Feature")\n'
+
+    if dependency_file_len > int(insert_line):
+        dependency_file.insert(insert_line, append_code)
+    
+    return dependency_file
+
+def write_file_at(file_path, update_file):
+    with open(file_path, 'w') as file:
+        file.writelines(update_file)
 
 
 print('Input new feature name ', end=': ', flush=True)
 feature_name = sys.stdin.readline().replace("\n", "")
+feature_name = feature_name.replace("Feature", "")
 
 print('Include demo? (Y or N, default = N) ', end=': ', flush=True)
 has_demo = sys.stdin.readline().replace("\n", "").upper() == "Y"
@@ -149,3 +176,6 @@ root_path = os.getcwd()
 os.chdir(root_path + '/Projects/Features')
 
 make_new_feature(feature_name, has_demo)
+write_created_feature_on_dependency_project(feature_name)
+
+print(f'Created {feature_name}Feature Module')
