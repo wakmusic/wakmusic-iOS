3ee09d983af03a8e1216044c1723e20129c3cc85
yongbeomkwak <yongbeomkwak@gmail.com>
2023-01-04 22:50:25 +0900

    :sparkles: :: 검색창 포커싱 여부에 따른 색 변경
    - RxCocoa의 controlEvent을 이용

--- Projects/Features/SearchFeature/Sources/SearchViewController.swift
+++ Projects/Features/SearchFeature/Sources/SearchViewController.swift
@@ -1,12 +1,16 @@
 import UIKit
 import Utility
 import DesignSystem
+import RxCocoa
+import RxSwift
 
 public final class SearchViewController: UIViewController, ViewControllerFromStoryBoard {
 
     @IBOutlet weak var searchImageView:UIImageView!
     @IBOutlet weak var searchTextFiled:UITextField!
     @IBOutlet weak var cancelButton:UIButton!
+    @IBOutlet weak var searchContentView:UIView!
+    let disposeBag = DisposeBag()
     
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,16 +37,64 @@ extension SearchViewController {
         //MARK: 검색 텍스트 필드
         
         let headerFontSize:CGFloat = 20
-        let placeHolderAttributes = [
+        let unFocusedplaceHolderAttributes = [
             NSAttributedString.Key.foregroundColor: DesignSystemAsset.GrayColor.gray400.color,
             NSAttributedString.Key.font : DesignSystemFontFamily.Pretendard.medium.font(size: headerFontSize)
-        ] // 플레이스홀더 폰트 및 color 설정
+        ] // 언 포커싱 플레이스홀더 폰트 및 color 설정
+        
+        let focusedplaceHolderAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font : DesignSystemFontFamily.Pretendard.medium.font(size: headerFontSize)
+        ] // 포커싱 플레이스홀더 폰트 및 color 설정
+        
+    
         
         self.searchTextFiled.font = DesignSystemFontFamily.Pretendard.medium.font(size: headerFontSize)
         self.searchTextFiled.textColor = .black
         // 텍스트 필드 폰트 및 색 설정
-        self.searchTextFiled.attributedPlaceholder = NSAttributedString(string: "검색어를 입력하세요.",attributes:placeHolderAttributes) //플레이스 홀더 설정
+        self.searchTextFiled.attributedPlaceholder = NSAttributedString(string: "검색어를 입력하세요.",attributes:unFocusedplaceHolderAttributes) //플레이스 홀더 설정
         self.searchTextFiled.borderStyle = .none // 텍스트 필드 테두리 제거
+        
+        
+        // MARK: 검색바 포커싱 시작 종료
+        self.searchTextFiled.rx.controlEvent([.editingDidEnd])
+            .asObservable()
+            .subscribe { [weak self]  _ in
+                
+                self?.view.backgroundColor = .white
+                self?.searchContentView.backgroundColor = .white
+                
+                self?.searchTextFiled.textColor = .black
+                self?.searchTextFiled.attributedPlaceholder = NSAttributedString(string: "검색어를 입력하세요.",attributes:unFocusedplaceHolderAttributes) //플레이스 홀더 설정
+                
+                self?.searchImageView.tintColor = DesignSystemAsset.GrayColor.gray400.color
+                
+            
+                self?.searchTextFiled.alpha = 0
+
+            }.disposed(by: self.disposeBag)
+    
+        
+        // MARK: 검색바 포커싱 시작
+        self.searchTextFiled.rx.controlEvent([.editingDidBegin])
+            .asObservable()
+            .subscribe { [weak self]  _ in
+                
+                self?.view.backgroundColor = DesignSystemAsset.PrimaryColor.point.color
+                self?.searchContentView.backgroundColor = DesignSystemAsset.PrimaryColor.point.color
+                
+                
+                self?.searchTextFiled.textColor = .white
+                self?.searchTextFiled.attributedPlaceholder = NSAttributedString(string: "검색어를 입력하세요.",attributes:focusedplaceHolderAttributes) //플레이스 홀더 설정
+                
+                
+                self?.searchImageView.tintColor = .white
+                
+                self?.searchTextFiled.alpha = 1
+                
+            }.disposed(by: self.disposeBag)
+        
+        
         
         
         //MARK: 검색 취소 버튼
@@ -52,5 +104,10 @@ extension SearchViewController {
         self.cancelButton.layer.borderColor =  DesignSystemAsset.GrayColor.gray200.color.cgColor
         self.cancelButton.layer.borderWidth = 1
         self.cancelButton.backgroundColor = .white
+        
+        
+        
+        
+        
     }
 }
