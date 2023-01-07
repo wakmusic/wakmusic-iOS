9e7ba1f459c376101b6c15e4c6aeb5c6e0138e59
KTH <pikagreen@nate.com>
2023-01-07 13:31:57 +0900

    💄 :: 왁뮤팀 추천 플레이 리스트 추가 (xib)

--- Projects/Features/HomeFeature/Sources/HomeViewController.swift
+++ Projects/Features/HomeFeature/Sources/HomeViewController.swift
@@ -5,6 +5,7 @@ import PanModal
 
 public final class HomeViewController: UIViewController, ViewControllerFromStoryBoard {
 
+    @IBOutlet weak var tempView: UIView!
     @IBOutlet weak var gradationImageView: UIImageView! {
         didSet {
             gradationImageView.image = DesignSystemAsset.Home.gradationBg.image
@@ -14,7 +15,7 @@ public final class HomeViewController: UIViewController, ViewControllerFromStory
     public override func viewDidLoad() {
         super.viewDidLoad()
 
-        DEBUG_LOG("\(Self.self) viewDidLoad")
+        configurePlayList()
     }
 
     public static func viewController() -> HomeViewController {
@@ -29,5 +30,31 @@ public final class HomeViewController: UIViewController, ViewControllerFromStory
         )
         let viewController: PanModalPresentable.LayoutType = textPopupViewController
         self.presentPanModal(viewController)
+    }
+}
+
+extension HomeViewController {
+    private func configurePlayList() {
+        
+        let dataSource = [RecommendPlayListDTO(title: "고멤가요제", image: DesignSystemAsset.RecommendPlayList.gomemSongFestival.image),
+                          RecommendPlayListDTO(title: "연말공모전", image: DesignSystemAsset.RecommendPlayList.competition.image),
+                          RecommendPlayListDTO(title: "상콘 OST", image: DesignSystemAsset.RecommendPlayList.situationalplayOST.image),
+                          RecommendPlayListDTO(title: "힙합 SWAG", image: DesignSystemAsset.RecommendPlayList.hiphop.image),
+                          RecommendPlayListDTO(title: "캐롤", image: DesignSystemAsset.RecommendPlayList.carol.image),
+                          RecommendPlayListDTO(title: "노동요", image: DesignSystemAsset.RecommendPlayList.workSong.image)]
+        
+        let recommendView = RecommendPlayListView(frame: CGRect(x: 0,
+                                                                y: 0,
+                                                                width: APP_WIDTH(),
+                                                                height: RecommendPlayListView.getViewHeight(model: dataSource)))
+        recommendView.dataSource = dataSource
+        recommendView.delegate = self
+        self.tempView.addSubview(recommendView)
+    }
+}
+
+extension HomeViewController: RecommendPlayListViewDelegate {
+    func itemSelected(model: RecommendPlayListDTO) {
+        DEBUG_LOG(model)
     }
 }
