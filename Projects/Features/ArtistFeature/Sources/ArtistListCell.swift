464ce93258195149c3d6e8b6b7817542df64b217
KTH <pikagreen@nate.com>
2023-01-07 19:19:08 +0900

    💄 :: 아티스트 리스트 UI 일부 구현

--- Projects/Features/ArtistFeature/Sources/ArtistListCell.swift
+++ Projects/Features/ArtistFeature/Sources/ArtistListCell.swift
@@ -17,32 +17,17 @@ class ArtistListCell: UICollectionViewCell {
 
 extension ArtistListCell {
 
-//    func update(model: Model.ArtistList) {
-//
-//        self.contentView.alpha = (model.isHiddenItem ?? false) ? 0 : 1
-//
-//        let sideSpace: CGFloat = 8.0
-//        let width: CGFloat = (APP_WIDTH() - ((sideSpace * 2.0) + 40.0)) / 3.0
-//
-//        bgView.layer.cornerRadius = width / 2
-//        bgView.clipsToBounds = true
-//        bgView.backgroundColor = colorFromRGB(model.color)
-//
-//        blurView.layer.cornerRadius = bgView.layer.cornerRadius
-//        blurView.clipsToBounds = true
-//
-//        artistLabel.text = model.short
-//
-//        let type: String = "full"
-//        let urlString: String = Router.baseURLString + "/static/artist/\(type)/\(model.id).png"
-//
-//        artistImageView.kf.setImage(with: URL(string: urlString), placeholder: nil,
-//                                     options: [.transition(.fade(0.2))], progressBlock: nil) { (result) in
-//
-//            switch result {
-//            case .success: break
-//            case .failure: break
-//            }
-//        }
-//    }
+    func update(model: ArtistListDTO) {
+
+        self.contentView.alpha = (model.isHiddenItem ?? false) ? 0 : 1
+
+        artistLabel.text = model.name
+        artistImageView.image = model.image
+    }
+}
+
+struct ArtistListDTO {
+    let name: String
+    let image: UIImage
+    var isHiddenItem: Bool? = false
 }
