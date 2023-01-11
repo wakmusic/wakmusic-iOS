126e585b91141c3500d7dd53d215877e93387567
youn9k <ace_lephant@naver.com>
2023-01-11 19:17:14 +0900

    :sparkles: :: 상태바 높이 구하는 유틸 메소드 추가

--- Projects/Modules/Utility/Sources/Utils.swift
+++ Projects/Modules/Utility/Sources/Utils.swift
@@ -17,6 +17,10 @@ public func APP_HEIGHT() -> CGFloat {
     return UIScreen.main.bounds.size.height
 }
 
+public func STATUS_BAR_HEGHIT() -> CGFloat {
+    return UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0
+}
+
 public func APP_VERSION() -> String {
     return Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? ""
 }
