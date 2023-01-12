439b6d527be7156cc0a82e1201797ee0fb5ae134
youn9k <ace_lephant@naver.com>
2023-01-12 21:57:27 +0900

    :art: :: PlayerView, MiniPlayerView로 분할

---
+++ Projects/Features/PlayerFeature/Sources/MiniPlayerView.swift
@@ -0,0 +1,26 @@
+//
+//  MiniPlayerView.swift
+//  PlayerFeature
+//
+//  Created by YoungK on 2023/01/12.
+//  Copyright © 2023 yongbeomkwak. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+import Utility
+import DesignSystem
+import SnapKit
+import Then
+
+final class MiniPlayerView: UIView {
+    override init(frame: CGRect) {
+            super.init(frame: frame)
+        }
+        
+        required init?(coder: NSCoder) {
+            super.init(coder: coder)
+        }
+        
+        
+}
