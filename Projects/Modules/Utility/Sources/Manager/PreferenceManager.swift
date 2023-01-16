e9ec16560d858af0f3227b1b3360af18dd78cf15
youn9k <ace_lephant@naver.com>
2023-01-16 17:16:53 +0900

    ⚡️ :: 유저디폴트 프로퍼티래퍼 추가

--- Projects/Modules/Utility/Sources/Manager/PreferenceManager.swift
+++ Projects/Modules/Utility/Sources/Manager/PreferenceManager.swift
@@ -29,3 +29,41 @@ public final class PreferenceManager {
         return records
     }
 }
+
+
+@propertyWrapper
+public final class UserDefaultWrapper<T: Codable> {
+    private let key: String
+    private let defaultValue: T?
+
+    init(key: String, defaultValue: T?) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+    
+    public var wrappedValue: T? {
+        get {
+            if let savedData = UserDefaults.standard.object(forKey: key) as? Data {
+                let decoder = JSONDecoder()
+                if let lodedObejct = try? decoder.decode(T.self, from: savedData) {
+                    return lodedObejct
+                }
+            }
+            return defaultValue
+        }
+        set {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(newValue) {
+                UserDefaults.standard.setValue(encoded, forKey: key)
+            }
+            subject.onNext(newValue)
+
+        }
+    }
+    
+    private lazy var subject = BehaviorSubject<T?>(value: wrappedValue)
+    public var projectedValue: Observable<T?> {
+        return subject.asObservable()
+    }
+    
+}
