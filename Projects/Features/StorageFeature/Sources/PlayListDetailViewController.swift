aa702a64ed1207ff03d79b21fa9c2af47a2ffec1
yongbeomkwak <yongbeomkwak@gmail.com>
2023-01-18 20:34:19 +0900

    :art: :: PlayListDetail 베이스 UI 바인딩

--- Projects/Features/StorageFeature/Sources/PlayListDetailViewController.swift
+++ Projects/Features/StorageFeature/Sources/PlayListDetailViewController.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 import Utility
+import DesignSystem
 
 class PlayListDetailViewController: UIViewController,ViewControllerFromStoryBoard {
-
+    
+    
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var moreButton: UIButton!
+    @IBOutlet weak var completeButton: UIButton!
+    @IBOutlet weak var playListImage: UIImageView!
+    @IBOutlet weak var playListNameLabel: UILabel!
+    @IBOutlet weak var playListCountLabel: UILabel!
+    @IBOutlet weak var editPlayNameButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var editStateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,17 +35,37 @@ class PlayListDetailViewController: UIViewController,ViewControllerFromStoryBoar
         return viewController
     }
     
-    
-    
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension PlayListDetailViewController{
+    
+    private func configureUI(){
+        
+        self.playListImage.image = DesignSystemAsset.PlayListTheme.theme0.image
+        
+        self.backButton.setImage(DesignSystemAsset.Navigation.back.image, for: .normal)
+    
+        self.moreButton.setImage(DesignSystemAsset.Storage.more.image, for: .normal)
+        
+        
+        self.completeButton.titleLabel?.text = "완료"
+        self.completeButton.titleLabel?.font = DesignSystemFontFamily.Pretendard.bold.font(size: 12)
+        self.completeButton.layer.cornerRadius = 4
+        self.completeButton.layer.borderColor =  DesignSystemAsset.PrimaryColor.point.color.cgColor
+        self.completeButton.layer.borderWidth = 1
+        self.completeButton.backgroundColor = .clear
+    
+        
+        self.editStateLabel.font = DesignSystemFontFamily.Pretendard.medium.font(size: 16)
+        
+        
+        self.playListCountLabel.font = DesignSystemFontFamily.Pretendard.light.font(size: 14)
+        self.playListCountLabel.textColor =  DesignSystemAsset.GrayColor.gray900.color.withAlphaComponent(0.6) // opacity 60%
+        
+        self.playListNameLabel.font  = DesignSystemFontFamily.Pretendard.bold.font(size: 20)
+        
+        
     }
-    */
-
+    
 }
