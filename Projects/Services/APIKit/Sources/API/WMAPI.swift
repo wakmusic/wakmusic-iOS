87fab81d90d8768c2a7705acd4407ca5519f3d1f
김대희 <0824dh@naver.com>
2023-01-24 19:37:36 +0900

    ✨ :: WMAPI protocol 설정

---
+++ Projects/Services/APIKit/Sources/API/WMAPI.swift
@@ -0,0 +1,45 @@
+import Moya
+import Foundation
+import ErrorModule
+import KeychainModule
+
+public protocol WMAPI: TargetType {
+    var domain: WMDomain { get }
+    var urlPath: String { get }
+    var errorMap: [Int: WMError] { get }
+}
+
+public extension WMAPI {
+    var baseURL: URL {
+        URL(string: "https://app-api.wakmusic.xyz")!
+    }
+
+    var path: String {
+        domain.asURLString + urlPath
+    }
+
+    var headers: [String: String]? {
+        ["Content-Type": "application/json"]
+    }
+
+    var validationType: ValidationType {
+        return .successCodes
+    }
+}
+
+public enum WMDomain: String {
+    case auth = "/api/auth"
+    case charts = "/api/charts"
+    case songs = "/api/songs"
+    case artist = "/api/artist"
+    case user = "/api/user"
+    case playlist = "/api/playlist"
+    case like = "/api/like"
+    case common = "/static"
+}
+
+extension WMDomain {
+    var asURLString: String {
+        "/\(self.rawValue)"
+    }
+}
