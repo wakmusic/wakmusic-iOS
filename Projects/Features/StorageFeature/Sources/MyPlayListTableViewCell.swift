2128f67b3cba9fcd7a9d8a8892012e6f0a562d24
yongbeomkwak <yongbeomkwak@gmail.com>
2023-01-27 15:34:37 +0900

    :poop: :: 리스트 가져오기, 만들기 헤더 배치

---
+++ Projects/Features/StorageFeature/Sources/MyPlayListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyPlayListTableViewCell.swift
+//  StorageFeature
+//
+//  Created by yongbeomkwak on 2023/01/27.
+//  Copyright © 2023 yongbeomkwak. All rights reserved.
+//
+
+import UIKit
+
+class MyPlayListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
