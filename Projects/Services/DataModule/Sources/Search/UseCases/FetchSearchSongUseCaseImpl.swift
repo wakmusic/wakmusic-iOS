48a9a003dfc493ff6c280cc12f6a0d9828dec755
yongbeomkwak <yongbeomkwak@gmail.com>
2023-02-07 18:20:13 +0900

    :sparkles: :: 레포지토리 및 UseCase 구현

---
+++ Projects/Services/DataModule/Sources/Search/UseCases/FetchSearchSongUseCaseImpl.swift
@@ -0,0 +1,21 @@
+import RxSwift
+import DataMappingModule
+import DomainModule
+import ErrorModule
+
+public struct FetchSearchSongUseCaseImpl: FetchSearchSongUseCase {
+    
+    private let searchRepository: any SearchRepository
+
+    public init(
+        searchRepository: SearchRepository
+    ) {
+        self.searchRepository = searchRepository
+    }
+    
+    public func execute(type: SearchType, keyword: String) -> RxSwift.Single<[SearchEntity]> {
+        searchRepository.fetchSearchSong(type: type, keyword: keyword)
+    }
+
+  
+}
