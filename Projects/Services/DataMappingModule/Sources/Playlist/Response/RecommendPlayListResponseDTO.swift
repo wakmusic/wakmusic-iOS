35cf13f5e43159f6b2a1fbc00e0d2c9142b2d3d4
yongbeomkwak <yongbeomkwak@gmail.com>
2023-02-10 00:11:58 +0900

    :zap: :: 추천 리스트 DTO

---
+++ Projects/Services/DataMappingModule/Sources/Playlist/Response/RecommendPlayListResponseDTO.swift
@@ -0,0 +1,15 @@
+//
+//  RecommendPlayListResponseDTO.swift
+//  DataMappingModuleTests
+//
+//  Created by yongbeomkwak on 2023/02/10.
+//  Copyright © 2023 yongbeomkwak. All rights reserved.
+//
+
+import Foundation
+
+
+public struct RecommendPlayListResponseDTO: Decodable {
+    public let id, title: String
+    public let `public`: Bool
+}
