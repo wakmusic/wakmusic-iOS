216d41d63d2ea41ba7526ab9bd0ce787edde5a39
yongbeomkwak <yongbeomkwak@gmail.com>
2023-02-10 00:17:50 +0900

    :truck: :: 오타 수정

---
+++ Projects/Services/DomainModule/Sources/Playlist/Entity/RecommendPlayListEntity.swift
@@ -0,0 +1,9 @@
+//
+//  RecommendPlayListEntity.swift
+//  DomainModuleTests
+//
+//  Created by yongbeomkwak on 2023/02/10.
+//  Copyright © 2023 yongbeomkwak. All rights reserved.
+//
+
+import Foundation
