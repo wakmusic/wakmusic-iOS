e965c75f94a51abb6212a095de9cb5b446569b90
yongbeomkwak <yongbeomkwak@gmail.com>
2023-02-17 23:20:38 +0900

    :sparkles: :: 프로필 레포,유즈케이스 프로토콜 생성

---
+++ Projects/Services/DomainModule/Sources/User/Repository/UserRepository.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistRepository.swift
+//  DomainModule
+//
+//  Created by KTH on 2023/02/08.
+//  Copyright © 2023 yongbeomkwak. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import DataMappingModule
+import ErrorModule
+
+public protocol UserRepository {
+    func setProfile(token:String,image:String) -> Completable
+
+}
