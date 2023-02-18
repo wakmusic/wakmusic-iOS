5e00402660a57acd6d31f1860a39da006cdef914
KTH <pikagreen@nate.com>
2023-02-19 00:16:10 +0900

    ⚡️:: 로그인 후 재접속 시 서버 데이터 동기화 로직 수정
    앱 삭제 후 재설치 한 경우는 제외 함.

--- Projects/Features/StorageFeature/Sources/ViewModels/AfterLoginStorageViewModel.swift
+++ Projects/Features/StorageFeature/Sources/ViewModels/AfterLoginStorageViewModel.swift
@@ -38,10 +38,15 @@ final public class AfterLoginViewModel:ViewModelType {
         
         let output = Output()
 
-        //MARK: 앱 접속 후 최초 1회는 서버에서 유저 정보를 가져와 동기화 한다.
-        fetchUserInfoUseCase.execute()
-            .asObservable()
+        //MARK: 앱 접속 후 최초 1회는 서버에서 유저 정보를 가져와 동기화 한다. 삭제 후 재설치 한 경우는 제외 함.
+        Utility.PreferenceManager.$userInfo
+            .filter { $0 != nil }
             .take(1)
+            .flatMap { [weak self] _ -> Observable<AuthUserInfoEntity> in
+                guard let self = self else { return Observable.empty() }
+                return self.fetchUserInfoUseCase.execute()
+                    .asObservable()
+            }
             .subscribe(onNext: {
                 PreferenceManager.shared.setUserInfo(
                     ID: AES256.encrypt(string: $0.id),
