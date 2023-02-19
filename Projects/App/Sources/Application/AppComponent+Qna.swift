c66c85eb0b0c054dde844c49011c873f75337983
yongbeomkwak <yongbeomkwak@gmail.com>
2023-02-19 19:35:53 +0900

    :sparkles: :: Qna API 정의

---
+++ Projects/App/Sources/Application/AppComponent+Qna.swift
@@ -0,0 +1,49 @@
+//
+//  AppComponent+Search.swift
+//  WaktaverseMusic
+//
+//  Created by yongbeomkwak on 2023/02/07.
+//  Copyright © 2023 yongbeomkwak. All rights reserved.
+//
+
+import DomainModule
+import DataModule
+import NetworkModule
+import CommonFeature
+import SignInFeature
+import StorageFeature
+
+//MARK: 변수명 주의
+// AppComponent 내 변수 == Dependency 내 변수  이름 같아야함
+
+
+public extension AppComponent {
+    
+
+    
+//    var remoteUserDataSource: any RemoteUserDataSource {
+//        shared {
+//            RemoteUserDataSourceImpl(keychain: keychain)
+//        }
+//    }
+//    
+//    var userRepository: any UserRepository {
+//        shared {
+//            UserRepositoryImpl(remoteUserDataSource: remoteUserDataSource)
+//        }
+//    }
+//    
+//    var fetchProfileListUseCase: any FetchProfileListUseCase{
+//        shared {
+//            FetchProfileListUseCaseImpl(userRepository: userRepository)
+//        }
+//    }
+//    
+//    var setProfileUseCase: any SetProfileUseCase {
+//        shared {
+//            SetProfileUseCaseImpl(userRepository: userRepository)
+//        }
+//    }
+    
+ 
+}
