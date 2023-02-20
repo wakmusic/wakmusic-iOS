2c9e4a29ede76d43cdd72d6f1eab40480d734f30
youn9k <ace_lephant@naver.com>
2023-02-20 17:25:37 +0900

    :art: :: 빌드 안되던 문제 수정 및 코드 가독성 개선

--- Projects/Features/PlayerFeature/Sources/ViewControllers/PlayerViewController.swift
+++ Projects/Features/PlayerFeature/Sources/ViewControllers/PlayerViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import SwiftUI
 import Utility
 import DesignSystem
 import SnapKit
@@ -18,65 +17,30 @@ import YoutubeKit
 
 public class PlayerViewController: UIViewController {
     private let disposeBag = DisposeBag()
-    //private var anyCancellable = Set<AnyCancellable>()
     var viewModel = PlayerViewModel()
-//    var youtubePlayer: YouTubePlayer?
     private var youtubePlayer: YTSwiftyPlayer!
     var playerView: PlayerView!
     var miniPlayerView: MiniPlayerView!
     
-    var youtubePlayerView = UIView()
-    let htmlStr = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <style>
-            html, body { margin: 0; padding: 0; width: 100%; height: 100%; background-color: #000000; }
-        </style>
-    </head>
-    <body>
-        <div id="player"></div>
-        <div id="explain"></div>
-        <script src="https://www.youtube.com/iframe_api" onerror="webkit.messageHandlers.onYouTubeIframeAPIFailedToLoad.postMessage('')"></script>
-        <script>
-            var player;
-            var time;
-            YT.ready(function() {
-                     player = new YT.Player('player', %@);
-                     webkit.messageHandlers.onYouTubeIframeAPIReady.postMessage('');
-                     function updateTime() {
-                         var state = player.getPlayerState();
-                         if (state == YT.PlayerState.PLAYING) {
-                            time = player.getCurrentTime();
-                            webkit.messageHandlers.onUpdateCurrentTime.postMessage(time);
-                         }
-                     }
-                     window.setInterval(updateTime, 500);
-                     });
-                     function onReady(event) {
-                         webkit.messageHandlers.onReady.postMessage('');
-                     }
-        function onStateChange(event) {
-            webkit.messageHandlers.onStateChange.postMessage(event.data);
+    var youtubePlayerView = UIView().then {
+        $0.isHidden = false
+    }
+    
+    public override func loadView() {
+        super.loadView()
+        playerView = PlayerView(frame: self.view.frame)
+        miniPlayerView = MiniPlayerView(frame: self.view.frame)
+        miniPlayerView.layer.opacity = 0
+        self.view.addSubview(playerView)
+        self.view.addSubview(miniPlayerView)
+        self.view.addSubview(youtubePlayerView)
+        self.youtubePlayerView.snp.makeConstraints {
+            $0.centerX.centerY.equalTo(self.playerView.thumbnailImageView)
+            $0.width.equalTo(320)
+            $0.height.equalTo(180)
         }
-        function onPlaybackQualityChange(event) {
-            webkit.messageHandlers.onPlaybackQualityChange.postMessage(event.data);
-        }
-        function onPlaybackRateChange(event) {
-            webkit.messageHandlers.onPlaybackRateChange.postMessage(event.data);
-        }
-        function onError(event) {
-            webkit.messageHandlers.onError.postMessage(event.data);
-        }
-        function onApiChange(event) {
-            webkit.messageHandlers.onApiChange.postMessage(event.data);
-        }
-        </script>
-    </body>
-    </html>
-
-    """
-
+    }
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
@@ -86,106 +50,69 @@ public class PlayerViewController: UIViewController {
     }
     
     func setupYoutubePlayer() {
-        //        let configuration = YouTubePlayer.Configuration(
-        //            autoPlay: true,
-        //            showControls: false,
-        //            loopEnabled: true,
-        //            showRelatedVideos: false
-        //        )
-        //
-        //        self.youtubePlayer = YouTubePlayer(
-        //            source: .video(id: "fgSXAKsq-Vo"),
-        //            configuration: configuration
-        //        )
+        self.youtubePlayer = YTSwiftyPlayer(
+            frame: .init(x: 0, y: 0, width: 320, height: 180),
+            playerVars: [
+                .playsInline(true),
+                .videoID("PVRkT5ZvXLU"),
+                .loopVideo(true),
+                .showRelatedVideo(false),
+                .autoplay(true)
+            ])
         
-        //        guard var youtubePlayer = self.youtubePlayer else { return }
+        youtubePlayer.delegate = self
+        self.youtubePlayerView.addSubview(youtubePlayer)
         
-        //        self.youtubePlayerView = YouTubePlayerHostingView(player: youtubePlayer)
-        
-        self.view.addSubview(self.youtubePlayerView)
-        
-//        self.youtubePlayerView.snp.makeConstraints {
-//            $0.centerX.centerY.equalTo(self.playerView.thumbnailImageView)
-//            $0.width.equalTo(320)
-//            $0.height.equalTo(180)
-//        }
-//        self.youtubePlayerView.isHidden = true
-//        self.youtubePlayer = YTSwiftyPlayer(
-//            frame: .init(x: 0, y: 0, width: 320, height: 190),
-//            playerVars: [
-//                .playsInline(true),
-//                .loopVideo(true),
-//                .showRelatedVideo(false),
-//                .autoplay(true)
-//            ])
-//        self.youtubePlayerView.addSubview(youtubePlayer)
-//        youtubePlayer.delegate = self
-//        // Load video player
-//        let playerPath = YoutubeKitResources.bundle
-//            .path(forResource: "player", ofType: "html")!
-//        let htmlString = (try? String(contentsOfFile: playerPath, encoding: .utf8)) ?? ""
-//        self.youtubePlayer.loadVideo(videoID: "fgSXAKsq-Vo")
-
-//        youtubePlayer.loadPlayerHTML(htmlStr)
-//        youtubePlayer.loadDefaultPlayer()
-
+        youtubePlayer.loadPlayerHTML(playerHtml)
     }
     
 }
 
 public extension PlayerViewController {
     func updateOpacity(value: Float) {
-//        playerView.layer.opacity = value
-//        miniPlayerView.layer.opacity = 1 - value
+        playerView.layer.opacity = value
+        miniPlayerView.layer.opacity = 1 - value
     }
 }
 
 private extension PlayerViewController {
     private func bindViewModel() {
-//        let input = PlayerViewModel.Input(
-//            viewWillAppearEvent: self.rx.methodInvoked(#selector(UIViewController.viewWillAppear)).map {_ in },
-//            closeButtonDidTapEvent: self.playerView.closeButton.rx.tap.asObservable(),
-//            playButtonDidTapEvent: self.playerView.playButton.rx.tap.asObservable(),
-//            prevButtonDidTapEvent: self.playerView.prevButton.rx.tap.asObservable(),
-//            nextButtonDidTapEvent: self.playerView.nextButton.rx.tap.asObservable(),
-//            repeatButtonDidTapEvent: self.playerView.repeatButton.rx.tap.asObservable(),
-//            shuffleButtonDidTapEvent: self.playerView.shuffleButton.rx.tap.asObservable(),
-//            likeButtonDidTapEvent: self.playerView.likeButton.rx.tap.asObservable(),
-//            addPlaylistButtonDidTapEvent: self.playerView.addPlayistButton.rx.tap.asObservable(),
-//            playlistButtonDidTapEvent: self.playerView.playistButton.rx.tap.asObservable(),
-//            miniExtendButtonDidTapEvent: self.miniPlayerView.extendButton.rx.tap.asObservable(),
-//            miniPlayButtonDidTapEvent: self.miniPlayerView.playButton.rx.tap.asObservable(),
-//            miniCloseButtonDidTapEvent: self.miniPlayerView.closeButton.rx.tap.asObservable()
-//        )
-//        let output = self.viewModel.transform(from: input)
-//
-//        output.didPlay
-//            .asDriver(onErrorJustReturn: false)
-//            .filter { $0 }
-//            .drive(onNext: { [weak self] _ in
-//                print("didPlay")
-//                self?.youtubePlayer.loadVideo(videoID: "dfcztPNevwg")
-//            })
-//            .disposed(by: disposeBag)
-//
-//        output.didClose
-//            .asDriver(onErrorJustReturn: false)
-//            .filter { $0 }
-//            .drive(onNext: { [weak self] _ in
-//                print("didClose")
-//            })
-//            .disposed(by: disposeBag)
+        let input = PlayerViewModel.Input(
+            viewWillAppearEvent: self.rx.methodInvoked(#selector(UIViewController.viewWillAppear)).map {_ in },
+            closeButtonDidTapEvent: self.playerView.closeButton.rx.tap.asObservable(),
+            playButtonDidTapEvent: self.playerView.playButton.rx.tap.asObservable(),
+            prevButtonDidTapEvent: self.playerView.prevButton.rx.tap.asObservable(),
+            nextButtonDidTapEvent: self.playerView.nextButton.rx.tap.asObservable(),
+            repeatButtonDidTapEvent: self.playerView.repeatButton.rx.tap.asObservable(),
+            shuffleButtonDidTapEvent: self.playerView.shuffleButton.rx.tap.asObservable(),
+            likeButtonDidTapEvent: self.playerView.likeButton.rx.tap.asObservable(),
+            addPlaylistButtonDidTapEvent: self.playerView.addPlayistButton.rx.tap.asObservable(),
+            playlistButtonDidTapEvent: self.playerView.playistButton.rx.tap.asObservable(),
+            miniExtendButtonDidTapEvent: self.miniPlayerView.extendButton.rx.tap.asObservable(),
+            miniPlayButtonDidTapEvent: self.miniPlayerView.playButton.rx.tap.asObservable(),
+            miniCloseButtonDidTapEvent: self.miniPlayerView.closeButton.rx.tap.asObservable()
+        )
+        let output = self.viewModel.transform(from: input)
+
+        output.didPlay
+            .asDriver(onErrorJustReturn: false)
+            .filter { $0 }
+            .drive(onNext: { [weak self] _ in
+                print("didPlay")
+                self?.youtubePlayer.loadVideo(videoID: "dfcztPNevwg")
+            })
+            .disposed(by: disposeBag)
+
+        output.didClose
+            .asDriver(onErrorJustReturn: false)
+            .filter { $0 }
+            .drive(onNext: { [weak self] _ in
+                print("didClose")
+            })
+            .disposed(by: disposeBag)
     }
     
     private func bindUI() {
-//        self.playerView.playButton.rx.tap.bind { _ in
-//            print("clicked")
-//            //self.youtubePlayer.load(source: .video(id: "1hcdQixxJdA"))
-//
-//           // self.youtubePlayer.play()
-//        }.disposed(by: disposeBag)
-        
-       // self.youtubePlayer.$source.sink { print($0?.id) }.store(in: &anyCancellable)
 
     }
 }
