30a7dad7582d5a06a7ba26ad62c07d5810dd3fe4
김대희 <0824dh@naver.com>
2023-02-22 00:53:50 +0900

    💩 :: chartcontentview 무지성 복붙

---
+++ Projects/Features/ChartFeature/Sources/ChartContentViewController.swift
@@ -0,0 +1,140 @@
+//
+//  ChartContentViewController.swift
+//  ChartFeature
+//
+//  Created by 김대희 on 2023/02/21.
+//  Copyright © 2023 yongbeomkwak. All rights reserved.
+//
+
+import UIKit
+import Utility
+import DesignSystem
+import RxSwift
+import RxCocoa
+import BaseFeature
+import CommonFeature
+import DataMappingModule
+import DomainModule
+
+public class ChartContentViewController: BaseViewController, ViewControllerFromStoryBoard {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIncidator: UIActivityIndicatorView!
+    
+    var disposeBag = DisposeBag()
+    
+//    private var viewModel: ArtistMusicContentViewModel!
+//    fileprivate lazy var input = ArtistMusicContentViewModel.Input(pageID: BehaviorRelay(value: 1))
+//    fileprivate lazy var output = viewModel.transform(from: input)
+
+    deinit {
+        DEBUG_LOG("\(Self.self) Deinit")
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+        bind()
+    }
+    
+    public static func viewController(
+//        viewModel: ChartContentViewController
+    ) -> ChartContentViewController {
+        let viewController = ChartContentViewController.viewController(storyBoardName: "Artist", bundle: Bundle.module)
+//        viewController.viewModel = viewModel
+        return viewController
+    }
+}
+
+extension ChartContentViewController {
+    
+    private func bind() {
+                
+        tableView.rx.setDelegate(self).disposed(by: disposeBag)
+
+//        output.dataSource
+//            .skip(1)
+//            .do(onNext: { [weak self] _ in
+//                guard let `self` = self else { return }
+//                DispatchQueue.main.async {
+//                    self.activityIncidator.stopAnimating()
+//                }
+//            })
+//            .bind(to: tableView.rx.items) { (tableView, index, model) -> UITableViewCell in
+//                let indexPath: IndexPath = IndexPath(row: index, section: 0)
+//                guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArtistMusicCell", for: indexPath) as? ArtistMusicCell else{
+//                    return UITableViewCell()
+//                }
+//                cell.update(model: model)
+//                return cell
+//            }.disposed(by: disposeBag)
+//
+//        tableView.rx.willDisplayCell
+//            .map { $1 }
+//            .withLatestFrom(output.dataSource, resultSelector: { (indexPath, datasource) -> (IndexPath, [ArtistSongListEntity]) in
+//                return (indexPath, datasource)
+//            })
+//            .filter{ (indexPath, datasources) -> Bool in
+//                return indexPath.item == datasources.count-1
+//            }
+//            .withLatestFrom(output.canLoadMore)
+//            .filter{ $0 }
+//            .map { _ in return () }
+//            .bind(to: rx.loadMore)
+//            .disposed(by: disposeBag)
+//
+//        tableView.rx.itemSelected
+//            .withLatestFrom(output.dataSource) { ($0, $1) }
+//            .subscribe(onNext: { [weak self] (indexPath, _) in
+//                guard let `self` = self else { return }
+//                self.tableView.deselectRow(at: indexPath, animated: true)
+////                let model = model[indexPath.row]
+//            }).disposed(by: disposeBag)
+
+    }
+    
+    private func configureUI() {
+        self.activityIncidator.startAnimating()
+        self.tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: APP_WIDTH(), height: 56))
+        self.tableView.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: 56, right: 0)
+    }
+}
+
+extension ChartContentViewController: UITableViewDelegate {
+
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 80
+    }
+
+    public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = PlayButtonGroupView(frame: CGRect(x: 0, y: 0, width: APP_WIDTH(), height: 80))
+        view.delegate = self
+        return view
+    }
+}
+
+extension ChartContentViewController: PlayButtonGroupViewDelegate{
+    public func pressPlay(_ event: PlayEvent) {
+        DEBUG_LOG(event)
+    }
+}
+
+//extension Reactive where Base: ChartContentViewController{
+//    var refresh: Binder<Void> {
+//        return Binder(base) { viewController, _ in
+//            viewController.input.pageID.accept(1)
+//        }
+//    }
+//
+//    var loadMore: Binder<Void> {
+//        return Binder(base) { viewController, _ in
+//            let pageID = viewController.input.pageID.value
+//            viewController.input.pageID.accept(pageID + 1)
+//        }
+//    }
+//}
