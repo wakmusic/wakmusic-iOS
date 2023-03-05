3ba6937da7c905a8321a1c7812c6ccdb3566ddf8
yongbeomkwak <yongbeomkwak@gmail.com>
2023-03-05 16:09:20 +0900

    :sparkles: :: 기능 제안 베이스 UI 구현 끝

---
+++ Projects/Features/StorageFeature/Sources/Components/SuggestFunctionComponent.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  StorageFeature
+//
+//  Created by yongbeomkwak on 2023/03/05.
+//  Copyright © 2023 yongbeomkwak. All rights reserved.
+//
+
+import Foundation
---
+++ Projects/Features/StorageFeature/Sources/ViewControllers/SuggestFunctionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SuggestFunctionViewController.swift
+//  StorageFeature
+//
+//  Created by yongbeomkwak on 2023/03/05.
+//  Copyright © 2023 yongbeomkwak. All rights reserved.
+//
+
+import UIKit
+
+class SuggestFunctionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
