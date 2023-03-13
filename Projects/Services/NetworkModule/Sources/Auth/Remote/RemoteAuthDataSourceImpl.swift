69e696bf4b1247b7cfec253f9588fc94c5f12735
김대희 <0824dh@naver.com>
2023-03-13 10:49:29 +0900

    ♻️ :: 로그인 api usecase 코드 정리

--- Projects/Services/NetworkModule/Sources/Auth/Remote/RemoteAuthDataSourceImpl.swift
+++ Projects/Services/NetworkModule/Sources/Auth/Remote/RemoteAuthDataSourceImpl.swift
@@ -5,12 +5,7 @@ import DomainModule
 import ErrorModule
 import Foundation
 
-
 public final class RemoteAuthDataSourceImpl: BaseRemoteDataSource<AuthAPI>, RemoteAuthDataSource {
-   
-    
-   
-
     public func fetchToken(id: String, type: ProviderType) -> Single<AuthLoginEntity> {
         request(.fetchToken(id: id, type: type))
             .map(AuthLoginResponseDTO.self)
