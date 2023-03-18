af9b7dcf56da7128da77acb4b4b32eb9f7dff970
KTH <pikagreen@nate.com>
2023-03-19 05:32:40 +0900

    ⚡️ :: BaseRemoteDataSource > timeout 10초로 변경

--- Projects/Services/NetworkModule/Sources/Base/BaseRemoteDataSource.swift
+++ Projects/Services/NetworkModule/Sources/Base/BaseRemoteDataSource.swift
@@ -43,7 +43,7 @@ public class BaseRemoteDataSource<API: WMAPI> {
 private extension BaseRemoteDataSource {
     func defaultRequest(_ api: API) -> Single<Response> {
         return provider.rx.request(api)
-            .timeout(.seconds(5), scheduler: MainScheduler.asyncInstance)
+            .timeout(.seconds(10), scheduler: MainScheduler.asyncInstance)
             .catch { error in
                 guard let errorCode = (error as? MoyaError)?.response?.statusCode else {
                     return Single.error(error)
