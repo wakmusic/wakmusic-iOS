7b368d10980e3e34c785a73f3174d2d77da5b782
yongbeomkwak <yongbeomkwak@gmail.com>
2023-03-20 10:02:34 +0900

    :sparkls: :: setEditing 적용

--- Projects/Features/StorageFeature/Sources/ViewControllers/MyPlayListViewController.swift
+++ Projects/Features/StorageFeature/Sources/ViewControllers/MyPlayListViewController.swift
@@ -93,8 +93,6 @@ extension MyPlayListViewController{
     
     private func configureUI()
     {
-        self.tableView.dragDelegate = self
-        self.tableView.dropDelegate = self
         bindRx()
     }
     
@@ -143,16 +141,13 @@ extension MyPlayListViewController{
                 if state.isEditing == false && state.force == false { // 정상적인 편집 완료 이벤트
                     self.input.runEditing.onNext(())
                 }
-                
-                
-                
-                self.tableView.dragInteractionEnabled = state.isEditing// true/false로 전환해 드래그 드롭을 활성화하고 비활성화 할 것입니다.
-                
+    
                 guard let parent = self.parent?.parent as? AfterLoginViewController else{
                     return
                 }
                 // 탭맨 쪽 편집 변경
                 parent.output.state.accept(EditState(isEditing: state.isEditing, force: true))
+                self.tableView.setEditing(state.isEditing, animated: true)
                 
         
                 
@@ -257,6 +252,14 @@ extension MyPlayListViewController:UITableViewDelegate{
         
     }
     
+    public func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none // 편집모드 시 왼쪽 버튼을 숨기려면 .none을 리턴합니다.
+    }
+    
+    public func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false // 편집모드 시 셀의 들여쓰기를 없애려면 false를 리턴합니다.
+    }
+    
 }
 
 extension MyPlayListViewController:MyPlayListHeaderViewDelegate{
@@ -270,76 +273,76 @@ extension MyPlayListViewController:MyPlayListHeaderViewDelegate{
     }    
 }
 
-extension  MyPlayListViewController: UITableViewDragDelegate {
-    public func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
-        
-        
-       input.sourceIndexPath.accept(indexPath)
-        let itemProvider = NSItemProvider(object: "1" as NSString)
-        let dragItem = UIDragItem(itemProvider: itemProvider)
-        return [dragItem]
-        
-        
-        // 애플의 공식 문서에서는 사용자가 특정 행을 드래그하는 것을 원하지 않으면 빈 배열을 리턴하라고 했는데,
-        //빈 배열을 리턴했을 때도 드래그가 가능했습니다. 이 부분은 더 자세히 알아봐야 할 것 같습니다.
-    }
-    
-    
-    
-    
-}
-
-extension  MyPlayListViewController: UITableViewDropDelegate {
-    
-    
-    // 손가락을 화면에서 뗐을 때. 드롭한 데이터를 불러와서 data source를 업데이트 하고, 필요하면 새로운 행을 추가한다.
-    public func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
-        
-        let destinationIndexPath: IndexPath
-
-                if let indexPath = coordinator.destinationIndexPath {
-                    destinationIndexPath = indexPath
-                } else {
-                    // Get last index path of table view.
-                    let section = tableView.numberOfSections - 1
-                    let row = tableView.numberOfRows(inSection: section)
-                    destinationIndexPath = IndexPath(row: row, section: section)
-                }
-        input.destIndexPath.accept(destinationIndexPath)
-        
-        
-        
-        var curr = output.dataSource.value
-        var tmp = curr[input.sourceIndexPath.value.row]
-        curr.remove(at: input.sourceIndexPath.value.row)
-        curr.insert(tmp, at: input.destIndexPath.value.row)
-        
-        output.dataSource.accept(curr)
-        
-        
-        DEBUG_LOG(destinationIndexPath)
-    }
-    
-    // 드래그할 떄 (손가락을 화면에 대고 있을 때)
-    public func tableView(_ tableView: UITableView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UITableViewDropProposal {
-        var dropProposal = UITableViewDropProposal(operation: .cancel)
-    
-       
-        // Accept only one drag item.
-        guard session.items.count == 1 else { return dropProposal }
-        
-        // The .move drag operation is available only for dragging within this app and while in edit mode.
-        if tableView.hasActiveDrag {
-            //            if tableView.isEditing {
-            dropProposal = UITableViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
-            //            }
-        } else {
-            // Drag is coming from outside the app.
-            dropProposal = UITableViewDropProposal(operation: .copy, intent: .insertAtDestinationIndexPath)
-        }
-        
-        
-        return dropProposal
-    }
-    
-}
+//extension  MyPlayListViewController: UITableViewDragDelegate {
+//    public func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+//
+//
+//       input.sourceIndexPath.accept(indexPath)
+//        let itemProvider = NSItemProvider(object: "1" as NSString)
+//        let dragItem = UIDragItem(itemProvider: itemProvider)
+//        return [dragItem]
+//
+//
+//        // 애플의 공식 문서에서는 사용자가 특정 행을 드래그하는 것을 원하지 않으면 빈 배열을 리턴하라고 했는데,
+//        //빈 배열을 리턴했을 때도 드래그가 가능했습니다. 이 부분은 더 자세히 알아봐야 할 것 같습니다.
+//    }
+//
+//
+//
+//
+//}
+//
+//extension  MyPlayListViewController: UITableViewDropDelegate {
+//
+//
+//    // 손가락을 화면에서 뗐을 때. 드롭한 데이터를 불러와서 data source를 업데이트 하고, 필요하면 새로운 행을 추가한다.
+//    public func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
+//
+//        let destinationIndexPath: IndexPath
+//
+//                if let indexPath = coordinator.destinationIndexPath {
+//                    destinationIndexPath = indexPath
+//                } else {
+//                    // Get last index path of table view.
+//                    let section = tableView.numberOfSections - 1
+//                    let row = tableView.numberOfRows(inSection: section)
+//                    destinationIndexPath = IndexPath(row: row, section: section)
+//                }
+//        input.destIndexPath.accept(destinationIndexPath)
+//
+//
+//
+//        var curr = output.dataSource.value
+//        var tmp = curr[input.sourceIndexPath.value.row]
+//        curr.remove(at: input.sourceIndexPath.value.row)
+//        curr.insert(tmp, at: input.destIndexPath.value.row)
+//
+//        output.dataSource.accept(curr)
+//
+//
+//        DEBUG_LOG(destinationIndexPath)
+//    }
+//
+//    // 드래그할 떄 (손가락을 화면에 대고 있을 때)
+//    public func tableView(_ tableView: UITableView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UITableViewDropProposal {
+//        var dropProposal = UITableViewDropProposal(operation: .cancel)
+//
+//
+//        // Accept only one drag item.
+//        guard session.items.count == 1 else { return dropProposal }
+//
+//        // The .move drag operation is available only for dragging within this app and while in edit mode.
+//        if tableView.hasActiveDrag {
+//            //            if tableView.isEditing {
+//            dropProposal = UITableViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+//            //            }
+//        } else {
+//            // Drag is coming from outside the app.
+//            dropProposal = UITableViewDropProposal(operation: .copy, intent: .insertAtDestinationIndexPath)
+//        }
+//
+//
+//        return dropProposal
+//    }
+//
+//}
