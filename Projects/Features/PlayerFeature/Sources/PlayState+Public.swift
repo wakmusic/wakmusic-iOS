110ca28b08a1569005e25b8d3f7e72bd81a15450
youn9k <ace_lephant@naver.com>
2023-03-27 21:30:58 +0900

    ✨ :: 노래 실행 PlayState 메소드 추가 및 주석

--- Projects/Features/PlayerFeature/Sources/PlayState+Public.swift
+++ Projects/Features/PlayerFeature/Sources/PlayState+Public.swift
@@ -10,8 +10,22 @@ import Foundation
 import DomainModule
 
 public extension PlayState {
+    /// 주어진 곡을 재생목록에 중복되지 않게 추가하고 재생합니다.
     func loadAndAppendUniqueSong(_ song: SongEntity) {
         self.playList.appendIfUnique(item: song)
         self.load(at: song)
+    }
+    
+    /// 주어진 곡들을 재생목록에 중복되지 않게 추가하고 재생합니다.
+    /// - 먼저 주어진 곡들의 첫번째 곡을 재생하며, 이후의 곡들은 중복 여부를 검사하여 재생목록의 마지막에 추가합니다.
+    func loadAndAppendUniqueSongs(_ songs: [SongEntity]) {
+        if let firstSong = songs.first {
+            self.playList.appendIfUnique(item: firstSong)
+            self.load(at: firstSong)
+        }
+        songs.dropFirst().map { song in
+            self.playList.appendIfUnique(item: song)
+            self.load(at: song)
+        }
     }
 }
