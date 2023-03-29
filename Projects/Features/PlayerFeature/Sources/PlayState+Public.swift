1ce33a44dd3fef73c981a62e4fe79f5fdeafa659
youn9k <ace_lephant@naver.com>
2023-03-29 21:40:03 +0900

    ♻️ :: loadAndAppend 단일, 중복 메소드 통합

--- Projects/Features/PlayerFeature/Sources/PlayState+Public.swift
+++ Projects/Features/PlayerFeature/Sources/PlayState+Public.swift
@@ -10,15 +10,10 @@ import Foundation
 import DomainModule
 
 public extension PlayState {
-    /// 주어진 곡을 재생목록에 중복되지 않게 추가하고 재생합니다.
-    func loadAndAppendUniqueSong(_ song: SongEntity) {
-        self.playList.appendIfUnique(item: song)
-        self.load(at: song)
-    }
-    
-    /// 주어진 곡들을 재생목록에 중복되지 않게 추가하고 재생합니다.
-    /// - 먼저 주어진 곡들의 첫번째 곡을 재생하며, 이후의 곡들은 중복 여부를 검사하여 재생목록의 마지막에 추가합니다.
-    func loadAndAppendUniqueSongs(_ songs: [SongEntity]) {
+    /// 주어진 곡들을 재생목록에 추가하고 재생합니다.
+    /// - 먼저 주어진 곡들의 첫번째 곡을 재생하며, 이후의 곡들은 재생목록의 마지막에 추가합니다.
+    /// - Parameter duplicateAllowed: 재생목록 추가 시 중복 허용 여부 (기본값: false)
+    func loadAndAppendSongs(_ songs: [SongEntity], duplicateAllowed: Bool = false) {
         if let firstSong = songs.first {
             self.playList.appendIfUnique(item: firstSong)
             self.load(at: firstSong)
