51589a4dc29adfe118e828a031c9e4518c49ed5c
youn9k <ace_lephant@naver.com>
2023-03-31 20:39:28 +0900

    ♻️ :: PlayState Enum 한 파일로 합치기

--- Projects/Features/CommonFeature/Sources/PlayState/RepeatMode.swift
+++ Projects/Features/CommonFeature/Sources/PlayState/RepeatMode.swift
@@ -1,26 +1,9 @@
 //
 //  RepeatMode.swift
-//  PlayerFeature
+//  CommonFeature
 //
-//  Created by YoungK on 2023/03/25.
+//  Created by YoungK on 2023/03/31.
 //  Copyright © 2023 yongbeomkwak. All rights reserved.
 //
 
 import Foundation
-
-public enum RepeatMode {
-    case none // 반복 없음
-    case repeatAll // 전체 반복
-    case repeatOnce // 한곡 반복
-    
-    public    mutating func rotate() {
-        switch self {
-        case .none:
-            self = .repeatAll
-        case .repeatAll:
-            self = .repeatOnce
-        case .repeatOnce:
-            self = .none
-        }
-    }
-}
