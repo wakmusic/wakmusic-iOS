6d42a6de3d5bcf173d84ce3ceeaf03f0318ff0b0
김대희 <0824dh@naver.com>
2023-04-01 14:25:11 +0900

    ✨ :: PKCEManager 사용하여 구글에서  access_token 얻기

--- Projects/Modules/Utility/Sources/Manager/GoogleLoginManager.swift
+++ Projects/Modules/Utility/Sources/Manager/GoogleLoginManager.swift
@@ -12,26 +12,31 @@ import Alamofire
 import RxSwift
 import RxRelay
 
+// MARK: - Delegate protocol
 public protocol GoogleOAuthLoginDelegate: AnyObject {
     func requestGoogleAccessToken(_ code: String)
 }
 
+// MARK: - Google Error
 public enum WMGoogleError: Error {
     case decodeError
     case internalError
 }
 
 public class GoogleLoginManager {
+    // MARK: - 변수 선언
     private let googleURL = "https://accounts.google.com/o/oauth2/v2/auth"
     private let accessTokenGoogleURL = "https://oauth2.googleapis.com"
     private let googleClientID = "715762772031-t7fpm1c6eeccfrcmmo75412kvbljtdhf.apps.googleusercontent.com"
-    private let googleSecretKey = "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"
-    private let scope = "https://www.googleapis.com/auth/userinfo.email"
+    private let googleSecretKey = "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"
+    private let scope = "profile"
 
+    private let pkce = try? PKCE()
     public static let shared = GoogleLoginManager()
     private let disposeBag = DisposeBag()
     public weak var googleOAuthLoginDelegate: GoogleOAuthLoginDelegate?
 
+    // MARK: - 사파리로 로그인 URL 보내기
     public func googleLoginRequest() {
         var components = URLComponents(string: googleURL)
 
@@ -39,24 +44,29 @@ public class GoogleLoginManager {
         let responseType = URLQueryItem(name: "response_type", value: "code")
         let redirectURI = URLQueryItem(name: "redirect_uri", value: "\(REDIRECT_URI()):redirect_uri_path")
         let clientID = URLQueryItem(name: "client_id", value: googleClientID)
-
-        components?.queryItems = [scope, responseType, redirectURI, clientID]
+        let codeChallenge = URLQueryItem(name: "code_challenge", value: pkce?.codeChallenge)
+        let codeChallengeMethod = URLQueryItem(name: "code_challenge_method", value: "S256")
+        
+        components?.queryItems = [scope, responseType, codeChallenge, redirectURI, clientID, codeChallengeMethod]
         if let url = components?.url, UIApplication.shared.canOpenURL(url) {
+            print(url)
             UIApplication.shared.open(url)
         }
     }
 
+    // MARK: - URL 뒤에 스코프로 온 Token catch
     public func getGoogleToken(_ url: URL) {
         let components = URLComponents(string: "\(url)")
         let items = components?.queryItems ?? []
         googleOAuthLoginDelegate?.requestGoogleAccessToken(items[0].value ?? "")
     }
 
+    // MARK: - Google Oauth 서버로 access_token get하기
     public func getGoogleOAuthToken(_ code: String) async throws -> Data {
         let parameters: Parameters = [
             "client_id": googleClientID,
-            "client_secret": googleSecretKey,
             "code": code,
+            "code_verifier": pkce?.codeVerifier ?? "",
             "redirect_uri": "\(REDIRECT_URI()):redirect_uri_path",
             "grant_type": "authorization_code"
         ]
@@ -68,7 +78,7 @@ public class GoogleLoginManager {
                 parameters: parameters
             )
             .responseData { response in
-                DEBUG_LOG(String(data: response.request?.httpBody ?? Data(), encoding: .utf8))
+                DEBUG_LOG(String(data: response.request?.httpBody ?? Data(), encoding: .utf8) ?? Data())
                 switch response.result {
                 case .success(let data):
                     continuation.resume(returning: data)
---
+++ Projects/Modules/Utility/Sources/Manager/PKCEManager.swift
@@ -0,0 +1,71 @@
+//
+//  PKCEManager.swift
+//  Utility
+//
+//  Created by 김대희 on 2023/03/30.
+//  Copyright © 2023 yongbeomkwak. All rights reserved.
+//
+
+import Foundation
+import CryptoKit
+
+struct PKCE {
+    typealias PKCECode = String
+
+    let codeVerifier: PKCECode // 임의로 생성한 ASCII 문자
+    let codeChallenge: PKCECode // 임의로 생성한 codeVerifier Base64URL 인코딩된 SHA256 해시
+    
+    init() throws {
+        codeVerifier = PKCE.generateCodeVerifier()
+        codeChallenge = try PKCE.codeChallenge(fromVerifier: codeVerifier)
+    }
+    
+    static func codeChallenge(fromVerifier verifier: PKCECode) throws -> PKCECode {
+        guard let verifierData = verifier.data(using: .ascii) else { throw PKCEError.improperlyFormattedVerifier }
+        
+        let challengeHashed = SHA256.hash(data: verifierData)
+        let challengeBase64Encoded = Data(challengeHashed).base64URLEncodedString
+        
+        return challengeBase64Encoded
+    }
+
+    static func generateCodeVerifier() -> PKCECode {
+        do {
+            let rando = try PKCE.generateCryptographicallySecureRandomOctets(count: 32)
+            return Data(bytes: rando, count: rando.count).base64URLEncodedString
+        } catch {
+            return generateBase64RandomString(ofLength: 43)
+        }
+    }
+
+    private static func generateCryptographicallySecureRandomOctets(count: Int) throws -> [UInt8] {
+        var octets = [UInt8](repeating: 0, count: count)
+        let status = SecRandomCopyBytes(kSecRandomDefault, octets.count, &octets)
+        
+        if status == errSecSuccess {
+            return octets
+        } else {
+            throw PKCEError.failedToGenerateRandomOctets
+        }
+    }
+    
+    private static func generateBase64RandomString(ofLength length: UInt8) -> PKCECode {
+        let base64 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        return String((0..<length).map{ _ in base64.randomElement()! })
+    }
+    
+    enum PKCEError: Error {
+        case failedToGenerateRandomOctets
+        case improperlyFormattedVerifier
+    }
+}
+
+extension Data {
+    var base64URLEncodedString: String {
+        base64EncodedString()
+            .replacingOccurrences(of: "=", with: "") // Remove any trailing '='s
+            .replacingOccurrences(of: "+", with: "-") // 62nd char of encoding
+            .replacingOccurrences(of: "/", with: "_") // 63rd char of encoding
+            .trimmingCharacters(in: .whitespaces)
+    }
+}
