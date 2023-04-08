837bc09f70ad6feaeeb1f4f4337fc31e8e75a4b2
KTH <pikagreen@nate.com>
2023-04-09 00:52:48 +0900

    ✨ :: 홈 > 블러 살짝 수정

--- Projects/Features/HomeFeature/Sources/ViewControllers/HomeViewController.swift
+++ Projects/Features/HomeFeature/Sources/ViewControllers/HomeViewController.swift
@@ -21,6 +21,7 @@ public final class HomeViewController: BaseViewController, ViewControllerFromSto
     @IBOutlet weak var chartContentView: UIView!
     @IBOutlet weak var chartBorderView: UIView!
     @IBOutlet weak var blurEffectView: UIVisualEffectView!
+    @IBOutlet weak var blurImageView: UIImageView!
     @IBOutlet weak var chartTitleLabel: UILabel!
     @IBOutlet weak var chartArrowImageView: UIImageView!
     @IBOutlet weak var chartAllListenButton: UIButton!
@@ -270,6 +271,12 @@ extension HomeViewController {
         blurEffectView.backgroundColor = UIColor.white.withAlphaComponent(0.4)
         blurEffectView.clipsToBounds = true
         
+        blurImageView.layer.cornerRadius = 12
+        blurImageView.contentMode = .scaleAspectFill
+        blurImageView.image = DesignSystemAsset.Home.blurBg.image
+        blurImageView.alpha = 0.8
+        blurImageView.clipsToBounds = true
+
         let mainTitleLabelAttributedString = NSMutableAttributedString(
             string: "왁뮤차트 TOP100",
             attributes: [.font: DesignSystemFontFamily.Pretendard.bold.font(size: 16),
