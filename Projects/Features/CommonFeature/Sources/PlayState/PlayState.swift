08a0ec126764da097e3e06b736f6a15691013a2c
youn9k <ace_lephant@naver.com>
2023-05-07 22:54:17 +0900

    ✨ :: append 수정 전

--- Projects/Features/CommonFeature/Sources/PlayState/PlayState.swift
+++ Projects/Features/CommonFeature/Sources/PlayState/PlayState.swift
@@ -26,27 +26,17 @@ final public class PlayState {
     private var subscription = Set<AnyCancellable>()
     
     init() {
-        let playedList = RealmManager.shared.realm.objects(PlayedLists.self)
-            .toArray(type: PlayedLists.self)
-            .map { PlayListItem(item:
-                SongEntity(
-                    id: $0.id,
-                    title: $0.title,
-                    artist: $0.artist,
-                    remix: $0.remix,
-                    reaction: $0.reaction,
-                    views: $0.views,
-                    last: $0.last,
-                    date: $0.date)
-            )}
-        
-        playList = PlayList(list: playedList)
+        playList = PlayList()
         progress = PlayProgress()
         state = .unstarted
         repeatMode = .none
         shuffleMode = .off
         player = YouTubePlayer(configuration: .init(autoPlay: false, showControls: false, showRelatedVideos: false))
-                
+        
+        let playedList = fetchPlayListFromLocalDB()
+        playList.list = playedList
+        currentSong = playedList.first?.item
+        
         player.playbackStatePublisher.sink { [weak self] state in
             guard let self = self else { return }
             self.state = state
@@ -62,7 +52,11 @@ final public class PlayState {
             self.progress.endProgress = duration
         }.store(in: &subscription)
         
-        playList.listChanged.sink { playListItems in
+        Publishers.Merge3(
+            playList.listAppended,
+            playList.listRemoved,
+            playList.listReordered
+        ).sink { playListItems in
             let allPlayedLists = RealmManager.shared.realm.objects(PlayedLists.self)
             RealmManager.shared.deleteRealmDB(model: allPlayedLists)
             
@@ -78,8 +72,27 @@ final public class PlayState {
                     date: $0.item.date
                 )}
             RealmManager.shared.addRealmDB(model: playedList)
+            print("🎉 로컬DB에 저장됨")
+            
         }.store(in: &subscription)
         
+    }
+    
+    func fetchPlayListFromLocalDB() -> [PlayListItem] {
+        let playedList = RealmManager.shared.realm.objects(PlayedLists.self)
+            .toArray(type: PlayedLists.self)
+            .map { PlayListItem(item:
+                SongEntity(
+                    id: $0.id,
+                    title: $0.title,
+                    artist: $0.artist,
+                    remix: $0.remix,
+                    reaction: $0.reaction,
+                    views: $0.views,
+                    last: $0.last,
+                    date: $0.date)
+            )}
+        return playedList
     }
     
 }
