a3982734df37e60e68e7e139dae746c13e7083e2
youn9k <ace_lephant@naver.com>
2023-05-10 23:29:46 +0900

    💩 :: 필요없는 코드 삭제

--- Projects/App/Sources/Application/SceneDelegate.swift
+++ Projects/App/Sources/Application/SceneDelegate.swift
@@ -32,21 +32,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     }
     func sceneDidEnterBackground(_ scene: UIScene) {
-        let allPlayedLists = RealmManager.shared.realm.objects(PlayedLists.self)
-        RealmManager.shared.deleteRealmDB(model: allPlayedLists)
         
-        let playedList =  PlayState.shared.playList.list.map {
-            PlayedLists(
-                id: $0.item.id,
-                title: $0.item.title,
-                artist: $0.item.artist,
-                remix: $0.item.remix,
-                reaction: $0.item.reaction,
-                views: $0.item.views,
-                last: $0.item.last,
-                date: $0.item.date
-            )}
-        RealmManager.shared.addRealmDB(model: playedList)
     }
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
