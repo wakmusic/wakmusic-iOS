244d04547ad0f63771b635717abe8279488d39ef
KTH <pikagreen@nate.com>
2023-05-28 01:59:55 +0900

    ✨ ::  로그인 전 화면 > 텍스트 자간 적용

--- Projects/Features/SignInFeature/Sources/Views/LoginUIExtension.swift
+++ Projects/Features/SignInFeature/Sources/Views/LoginUIExtension.swift
@@ -30,8 +30,10 @@ extension LoginViewController {
         for attr in loginAttributedString{
             attr.addAttributes([
                 .font: DesignSystemFontFamily.Pretendard.medium.font(size: 16),
-                .foregroundColor: DesignSystemAsset.GrayColor.gray900.color],
-                range: NSRange(location: 0, length: attr.string.count))
+                .foregroundColor: DesignSystemAsset.GrayColor.gray900.color,
+                .kern: -0.5],
+                range: NSRange(location: 0, length: attr.string.count)
+            )
         }
 
         let superViewArr:[UIView] = [naverSuperView,googleSuperView,appleSuperView]
@@ -50,21 +52,26 @@ extension LoginViewController {
         appleLoginButton.setAttributedTitle(loginAttributedString[2], for: .normal)
     }
     private func configureService() {
-        let serviceAttributedString = NSMutableAttributedString
-            .init(string: "서비스 이용약관")
+        let appAttributedString = NSMutableAttributedString
+            .init(string: "왁타버스 뮤직")
         
-        serviceAttributedString.addAttributes([
-            .font: DesignSystemFontFamily.Pretendard.medium.font(size: 14),
-            .foregroundColor: DesignSystemAsset.GrayColor.gray600.color],
-            range: NSRange(location: 0, length: serviceAttributedString.string.count))
-        
-        let privacyAttributedString = NSMutableAttributedString
-            .init(string: "개인정보처리방침")
-        
-        privacyAttributedString.addAttributes([
-            .font: DesignSystemFontFamily.Pretendard.medium.font(size: 14),
-            .foregroundColor: DesignSystemAsset.GrayColor.gray600.color],
-            range: NSRange(location: 0, length: privacyAttributedString.string.count)
+        appAttributedString.addAttributes([
+            .font: DesignSystemFontFamily.Pretendard.medium.font(size: 20),
+            .foregroundColor: DesignSystemAsset.GrayColor.gray900.color,
+            .kern: -0.5],
+            range: NSRange(location: 0, length: appAttributedString.string.count)
         )
+        appNameLabel.attributedText = appAttributedString
+        
+        let descriptionAttributedString = NSMutableAttributedString
+            .init(string: "페이지를 이용하기 위해 로그인이 필요합니다.")
+        
+        descriptionAttributedString.addAttributes([
+            .font: DesignSystemFontFamily.Pretendard.light.font(size: 14),
+            .foregroundColor: DesignSystemAsset.GrayColor.gray600.color,
+            .kern: -0.5],
+            range: NSRange(location: 0, length: descriptionAttributedString.string.count)
+        )
+        descriptionLabel.attributedText = descriptionAttributedString
     }
 }
