ffd44222566325e01f99a53b6fc70c61afb27843
KTH <pikagreen@nate.com>
2023-06-06 00:13:53 +0900

    🧱 :: 프로젝트 설정의 Build 넘버를 따라가도록 Info.plist 환경변수 설정
    빌드넘버는 Tuist 설정에서 변경합니다.

--- Tuist/ProjectDescriptionHelpers/Environment.swift
+++ Tuist/ProjectDescriptionHelpers/Environment.swift
@@ -10,7 +10,7 @@ public enum Environment {
     public static let platform = Platform.iOS
     public static let baseSetting: SettingsDictionary = SettingsDictionary()
         .marketingVersion("2.0.0")
-        .currentProjectVersion("0")
+        .currentProjectVersion("10")
         .debugInformationFormat(DebugInformationFormat.dwarfWithDsym)
         .otherLinkerFlags(["-ObjC"])
 }
