fac6921fd20baad6ddcd22b57bc717311ea74bae
KTH <pikagreen@nate.com>
2023-06-11 14:24:12 +0900

    🔨 :: Tuist build version 수정

--- Tuist/ProjectDescriptionHelpers/Environment.swift
+++ Tuist/ProjectDescriptionHelpers/Environment.swift
@@ -10,7 +10,7 @@ public enum Environment {
     public static let platform = Platform.iOS
     public static let baseSetting: SettingsDictionary = SettingsDictionary()
         .marketingVersion("2.0.0")
-        .currentProjectVersion("11")
+        .currentProjectVersion("13")
         .debugInformationFormat(DebugInformationFormat.dwarfWithDsym)
         .otherLinkerFlags(["-ObjC"])
 }
