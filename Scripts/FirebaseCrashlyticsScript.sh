65586c56885a9ca29b4f246a98da4bf963cde03c
KTH <pikagreen@nate.com>
2023-06-13 04:03:51 +0900

    🔨 :: firebasecrashlytics 스크립트 수정

--- Scripts/FirebaseCrashlyticsScript.sh
+++ Scripts/FirebaseCrashlyticsScript.sh
@@ -1,4 +1,3 @@
 if [ "${CONFIGURATION}" != "Debug" ]; then
     "../Tuist/Dependencies/SwiftPackageManager/.build/checkouts/firebase-ios-sdk/Crashlytics/run"
-    "../Tuist/Dependencies/SwiftPackageManager/.build/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp ../Projects/App/Resources/GoogleService-Info.plist -p ios ${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}
 fi
