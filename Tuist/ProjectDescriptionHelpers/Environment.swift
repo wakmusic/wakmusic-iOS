44d3b2bba7145672bf011e190b3be268491d2485
KTH <pikagreen@nate.com>
2023-07-07 00:43:44 +0900

    🔖 :: Build v2.0.1(1)

--- Tuist/ProjectDescriptionHelpers/Environment.swift
+++ Tuist/ProjectDescriptionHelpers/Environment.swift
@@ -10,7 +10,7 @@ public enum Environment {
     public static let platform = Platform.iOS
     public static let baseSetting: SettingsDictionary = SettingsDictionary()
         .marketingVersion("2.0.1")
-        .currentProjectVersion("0")
+        .currentProjectVersion("1")
         .debugInformationFormat(DebugInformationFormat.dwarfWithDsym)
         .otherLinkerFlags(["-ObjC"])
         .bitcodeEnabled(false)
