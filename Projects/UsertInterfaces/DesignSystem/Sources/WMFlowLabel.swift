91569e0a674f62f2f05e85696f5e9d2551c59e0d
youn9k <ace_lephant@naver.com>
2023-11-25 16:26:50 +0900

    🚚 :: WMFlowLabel 파일 분리

--- Projects/UsertInterfaces/DesignSystem/Sources/WMFlowLabel.swift
+++ Projects/UsertInterfaces/DesignSystem/Sources/WMFlowLabel.swift
@@ -1,15 +1,15 @@
 //
-//  WMLabel.swift
+//  WMFlowLabel.swift
 //  DesignSystem
 //
-//  Created by YoungK on 11/22/23.
+//  Created by YoungK on 11/25/23.
 //  Copyright © 2023 yongbeomkwak. All rights reserved.
 //
 
 import UIKit
 import MarqueeLabel
 
-public final class WMLabel: UILabel {
+public final class WMFlowLabel: MarqueeLabel {
     public init(
         text: String = "",
         textColor: UIColor = .init(),
@@ -18,18 +18,25 @@ public final class WMLabel: UILabel {
         lineHeight: CGFloat? = nil,
         kernValue: Double? = nil,
         lineSpacing: CGFloat? = nil,
-        lineHeightMultiple: CGFloat? = nil
+        lineHeightMultiple: CGFloat? = nil,
+        leadingBuffer: CGFloat = 0,
+        trailingBuffer: CGFloat = 0,
+        animationDelay: CGFloat = 1,
+        animationSpeed: CGFloat = 30,
+        fadeLength: CGFloat = 3
     ) {
-        super.init(frame: .zero)
+        super.init(frame: .zero, rate: animationSpeed, fadeLength: fadeLength)
         self.text = text
         self.textColor = textColor
         self.font = .setFont(font)
         self.textAlignment = alignment
         self.setTextWithAttributes(lineHeight: lineHeight, kernValue: kernValue, lineSpacing: lineSpacing, lineHeightMultiple: lineHeightMultiple, alignment: alignment)
+        self.leadingBuffer = leadingBuffer
+        self.trailingBuffer = trailingBuffer
+        self.animationDelay = animationDelay
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
-
