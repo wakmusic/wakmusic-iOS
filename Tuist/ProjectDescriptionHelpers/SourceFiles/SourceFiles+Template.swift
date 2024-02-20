1ba58ab4da86d8554df2870144b1a98b554b879f
Hamp <yongbeomkwak@gmail.com>
2024-02-20 22:12:55 +0900

    :zap: :: SourceFiles 템플릿 확장

--- Tuist/ProjectDescriptionHelpers/SourceFiles/SourceFiles+Template.swift
+++ Tuist/ProjectDescriptionHelpers/SourceFiles/SourceFiles+Template.swift
@@ -1,8 +1,11 @@
-//
-//  SourceFiles+Template.swift
-//  ProjectDescriptionHelpers
-//
-//  Created by yongbeomkwak on 2/20/24.
-//
-
 import Foundation
+import ProjectDescription
+
+public extension SourceFilesList {
+    static let demoSources: SourceFilesList = "Demo/Sources/**"
+    static let interface: SourceFilesList = "Interface/**"
+    static let sources: SourceFilesList = "Sources/**"
+    static let testing: SourceFilesList = "Testing/**"
+    static let unitTests: SourceFilesList = "Tests/**"
+    static let uiTests: SourceFilesList = "UITests/**"
+}
