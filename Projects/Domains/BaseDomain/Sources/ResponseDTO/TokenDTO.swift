dd48f939c94e6220375488424c56faacf66c5eae
baegteun <baegteun@gmail.com>
2024-04-13 22:21:28 +0900

    :recycle: :: [#477] TokenDTO / JwtPlugin에서 Refresh API를 통한 ResponseBody도 대응

--- Projects/Domains/BaseDomain/Sources/ResponseDTO/TokenDTO.swift
+++ Projects/Domains/BaseDomain/Sources/ResponseDTO/TokenDTO.swift
@@ -10,8 +10,44 @@ import Foundation
 
 struct TokenDTO: Equatable, Decodable {
     let accessToken: String
+    let refreshToken: String?
+    let expiresIn: Int
 
-    enum CodingKeys: String, CodingKey {
-        case accessToken = "access_token"
+    enum DefaultCodingKeys: String, CodingKey {
+        case accessToken
+        case refreshToken
+        case expiresIn
+    }
+
+    enum RefreshableCodingKeys: String, CodingKey {
+        case token
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: DefaultCodingKeys.self)
+        let refreshableContainer = try decoder.container(keyedBy: RefreshableCodingKeys.self)
+
+        let accessToken: String
+        if let token = try? container.decode(String.self, forKey: .accessToken) {
+            accessToken = token
+        } else if let token = try? refreshableContainer.decode(String.self, forKey: .token) {
+            accessToken = token
+        } else {
+            throw Error.cannotDecodeAccessToken
+        }
+        self.accessToken = accessToken
+        self.refreshToken = try container.decodeIfPresent(String.self, forKey: .refreshToken)
+        self.expiresIn = try container.decode(Int.self, forKey: .expiresIn)
+    }
+
+    enum Error: Swift.Error, LocalizedError {
+        case cannotDecodeAccessToken
+
+        var errorDescription: String? {
+            switch self {
+            case .cannotDecodeAccessToken:
+                return "Cannot decode access token in data"
+            }
+        }
     }
 }
