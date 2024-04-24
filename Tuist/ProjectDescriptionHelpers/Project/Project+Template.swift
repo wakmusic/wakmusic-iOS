e1efdebc0cb6dbb7859e9fcbe39eb7e19e1dd2f1
baegteun <baegteun@gmail.com>
2024-04-24 23:34:45 +0900

    :bug: :: [#499] rename Scheme Demo모듈 이름

--- Tuist/ProjectDescriptionHelpers/Project/Project+Template.swift
+++ Tuist/ProjectDescriptionHelpers/Project/Project+Template.swift
@@ -177,11 +177,11 @@ extension Scheme {
         return Scheme(
             name: name,
             shared: true,
-            buildAction: .buildAction(targets: ["\(name)DemoApp"]),
+            buildAction: .buildAction(targets: ["\(name)Demo"]),
             testAction: .targets(
                 ["\(name)Tests"],
                 configuration: target,
-                options: .options(coverage: true, codeCoverageTargets: ["\(name)DemoApp"])
+                options: .options(coverage: true, codeCoverageTargets: ["\(name)Demo"])
             ),
             runAction: .runAction(configuration: target),
             archiveAction: .archiveAction(configuration: target),
