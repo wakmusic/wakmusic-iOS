8e0b0b87587de37e5ea544d1510ca6978de31581
baegteun <baegteun@gmail.com>
2024-04-24 20:15:00 +0900

    :seedling: :: [#483] MusicDetailFeature 모듈 추가

---
+++ Projects/Features/MusicDetailFeature/Project.swift
@@ -0,0 +1,19 @@
+import DependencyPlugin
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(
+    name: ModulePaths.Feature.MusicDetailFeature.rawValue,
+    targets: [
+        .interface(module: .feature(.MusicDetailFeature)),
+        .implements(module: .feature(.MusicDetailFeature), dependencies: [
+            .feature(target: .MusicDetailFeature, type: .interface)
+        ]),
+        .tests(module: .feature(.MusicDetailFeature), dependencies: [
+            .feature(target: .MusicDetailFeature)
+        ]),
+        .demo(module: .feature(.MusicDetailFeature), dependencies: [
+            .feature(target: .MusicDetailFeature)
+        ])
+    ]
+)
