0c4842235a9702690d6c72f06f852be12b92bfc0
baegteun <baegteun@gmail.com>
2024-04-27 18:14:16 +0900

    :bug: :: [#505] 색상 이름이 겹쳐서 생기는 색상 미일치 현상 해결

---
+++ Projects/UsertInterfaces/DesignSystem/Sources/AliasPrimaryV1+PrimaryV2.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+public extension DesignSystemAsset.PrimaryColor {
+    static var Sub1: DesignSystemColors { Self.v1Sub1 }
+    static var Sub2: DesignSystemColors { Self.v1Sub2 }
+    static var Sub3: DesignSystemColors { Self.v1Sub3 }
+}
