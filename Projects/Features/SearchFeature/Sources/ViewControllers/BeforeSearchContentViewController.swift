e3b7190044d2fab4a9a30a8b2379d99e6828e683
Hamp <yongbeomkwak@gmail.com>
2024-05-04 23:33:21 +0900

    :bricks: :: 포맷팅

--- Projects/Features/SearchFeature/Sources/ViewControllers/BeforeSearchContentViewController.swift
+++ Projects/Features/SearchFeature/Sources/ViewControllers/BeforeSearchContentViewController.swift
@@ -57,7 +57,7 @@ public final class BeforeSearchContentViewController: BaseViewController, ViewCo
         viewController.viewModel = viewModel
         return viewController
     }
-    
+
     deinit {
         DEBUG_LOG("❌ \(Self.self)")
     }
