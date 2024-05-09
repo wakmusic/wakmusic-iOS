0a574347b85d9097133be78c7cc9adf2487a513c
Hamp <yongbeomkwak@gmail.com>
2024-05-09 22:29:47 +0900

    :bricks: :: 포맷

--- Projects/Features/StorageFeature/Sources/ViewControllers/StorageViewController.swift
+++ Projects/Features/StorageFeature/Sources/ViewControllers/StorageViewController.swift
@@ -4,28 +4,23 @@ import DesignSystem
 import KeychainModule
 import Pageboy
 import PanModal
+import ReactorKit
 import RxSwift
+import SignInFeatureInterface
 import Tabman
 import UIKit
 import Utility
-import SignInFeatureInterface
-import ReactorKit
 
-final class StorageViewController: TabmanViewController, ViewControllerFromStoryBoard  ,EqualHandleTappedType, StoryboardView {
-    
-    
+final class StorageViewController: TabmanViewController, ViewControllerFromStoryBoard, EqualHandleTappedType,
+    StoryboardView {
     typealias Reactor = StorageReactor
-    
+
     var disposeBag: DisposeBag = DisposeBag()
-    
-    
+
     @IBOutlet weak var tabBarView: UIView!
     @IBOutlet weak var editButton: UIButton!
     @IBOutlet weak var myPlayListFakeView: UIView!
     @IBOutlet weak var favoriteFakeView: UIView!
-
-
-
 
     private var viewControllers: [UIViewController] = [UIViewController(), UIViewController()]
 
@@ -36,18 +31,13 @@ final class StorageViewController: TabmanViewController, ViewControllerFromStory
     private var textPopUpFactory: TextPopUpFactory!
     private var signInFactory: SignInFactory!
 
-
-
-
     override public func viewDidLoad() {
         super.viewDidLoad()
-  
     }
 
     override public func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
     }
-    
 
     /// 탭맨 페이지 변경 감지 함수
     override public func pageboyViewController(
@@ -72,20 +62,20 @@ final class StorageViewController: TabmanViewController, ViewControllerFromStory
             vc2.output.state.accept(state)
             editButton.isHidden = (vc2.output.dataSource.value.first?.items ?? []).isEmpty
         }
-        //output.state.accept(state)
+        // output.state.accept(state)
     }
 
     public static func viewController(
-      reactor: StorageReactor,
-      myPlayListComponent: MyPlayListComponent,
-      multiPurposePopUpFactory: MultiPurposePopUpFactory,
-      favoriteComponent: FavoriteComponent,
-      textPopUpFactory: TextPopUpFactory,
-      signInFactory: SignInFactory
-      
+        reactor: StorageReactor,
+        myPlayListComponent: MyPlayListComponent,
+        multiPurposePopUpFactory: MultiPurposePopUpFactory,
+        favoriteComponent: FavoriteComponent,
+        textPopUpFactory: TextPopUpFactory,
+        signInFactory: SignInFactory
+
     ) -> StorageViewController {
         let viewController = StorageViewController.viewController(storyBoardName: "Storage", bundle: Bundle.module)
-        
+
         viewController.reactor = reactor
 
         viewController.myPlayListComponent = myPlayListComponent
@@ -100,194 +90,185 @@ final class StorageViewController: TabmanViewController, ViewControllerFromStory
     deinit {
         DEBUG_LOG("❌ \(Self.self)")
     }
-    
+
     func bind(reactor: StorageReactor) {
         bindAction(reactor: reactor)
         bindState(reactor: reactor)
     }
-    
-    
 }
 
 extension StorageViewController {
-    
-    func bindAction(reactor: Reactor) {
-        
-    }
-    
-    func bindState(reactor: Reactor) {
-        
-    }
-    
+    func bindAction(reactor: Reactor) {}
+
+    func bindState(reactor: Reactor) {}
 }
 
 /*
-extension StorageViewController {
-    private func bindRx() {
-        output.state.subscribe { [weak self] state in
-            guard let self = self else {
-                return
-            }
+ extension StorageViewController {
+     private func bindRx() {
+         output.state.subscribe { [weak self] state in
+             guard let self = self else {
+                 return
+             }
 
-            let attr = NSMutableAttributedString(
-                string: state.isEditing ? "완료" : "편집",
-                attributes: [
-                    .font: DesignSystemFontFamily.Pretendard.bold.font(size: 12),
-                    .foregroundColor: state.isEditing ? DesignSystemAsset.PrimaryColor
-                        .point.color : DesignSystemAsset.GrayColor.gray400.color
-                ]
-            )
-            self.editButton.layer.borderColor = state.isEditing ? DesignSystemAsset.PrimaryColor.point.color
-                .cgColor : DesignSystemAsset.GrayColor.gray300.color.cgColor
-            self.editButton.setAttributedTitle(attr, for: .normal)
-            self.isScrollEnabled = !state.isEditing //  편집 시 , 옆 탭으로 swipe를 막기 위함
-            self.headerFakeView.isHidden = !state.isEditing
+             let attr = NSMutableAttributedString(
+                 string: state.isEditing ? "완료" : "편집",
+                 attributes: [
+                     .font: DesignSystemFontFamily.Pretendard.bold.font(size: 12),
+                     .foregroundColor: state.isEditing ? DesignSystemAsset.PrimaryColor
+                         .point.color : DesignSystemAsset.GrayColor.gray400.color
+                 ]
+             )
+             self.editButton.layer.borderColor = state.isEditing ? DesignSystemAsset.PrimaryColor.point.color
+                 .cgColor : DesignSystemAsset.GrayColor.gray300.color.cgColor
+             self.editButton.setAttributedTitle(attr, for: .normal)
+             self.isScrollEnabled = !state.isEditing //  편집 시 , 옆 탭으로 swipe를 막기 위함
+             self.headerFakeView.isHidden = !state.isEditing
 
-            if state.isEditing {
-                self.myPlayListFakeView.isHidden = self.currentIndex == 0
-                self.favoriteFakeView.isHidden = self.currentIndex == 1
-            } else {
-                self.myPlayListFakeView.isHidden = true
-                self.favoriteFakeView.isHidden = true
-            }
-        }.disposed(by: disposeBag)
+             if state.isEditing {
+                 self.myPlayListFakeView.isHidden = self.currentIndex == 0
+                 self.favoriteFakeView.isHidden = self.currentIndex == 1
+             } else {
+                 self.myPlayListFakeView.isHidden = true
+                 self.favoriteFakeView.isHidden = true
+             }
+         }.disposed(by: disposeBag)
 
-        editButton.rx.tap
-            .withLatestFrom(output.state)
-            .map { EditState(isEditing: !$0.isEditing, force: $0.force) }
-            .do(onNext: { [weak self] (state: EditState) in
-                guard let self = self else {
-                    return
-                }
+         editButton.rx.tap
+             .withLatestFrom(output.state)
+             .map { EditState(isEditing: !$0.isEditing, force: $0.force) }
+             .do(onNext: { [weak self] (state: EditState) in
+                 guard let self = self else {
+                     return
+                 }
 
-                // 프로필 편집 팝업을 띄운 상태에서 리스트 편집 시 > 프로필 편집 팝업을 제거
-                if self.editSheetView != nil {
-                    self.hideEditSheet()
-                }
+                 // 프로필 편집 팝업을 띄운 상태에서 리스트 편집 시 > 프로필 편집 팝업을 제거
+                 if self.editSheetView != nil {
+                     self.hideEditSheet()
+                 }
 
-                let nextState = EditState(isEditing: state.isEditing, force: false)
+                 let nextState = EditState(isEditing: state.isEditing, force: false)
 
-                if self.currentIndex ?? 0 == 0 {
-                    guard let vc = self.viewControllers[0] as? MyPlayListViewController else {
-                        return
-                    }
-                    vc.output.state.accept(nextState)
-                } else {
-                    guard let vc = self.viewControllers[1] as? FavoriteViewController else {
-                        return
-                    }
-                    vc.output.state.accept(nextState)
-                }
-            })
-            .bind(to: output.state)
-            .disposed(by: disposeBag)
+                 if self.currentIndex ?? 0 == 0 {
+                     guard let vc = self.viewControllers[0] as? MyPlayListViewController else {
+                         return
+                     }
+                     vc.output.state.accept(nextState)
+                 } else {
+                     guard let vc = self.viewControllers[1] as? FavoriteViewController else {
+                         return
+                     }
+                     vc.output.state.accept(nextState)
+                 }
+             })
+             .bind(to: output.state)
+             .disposed(by: disposeBag)
 
-        profileButton.rx.tap.subscribe(onNext: { [weak self] in
-            guard let self = self else { return }
-            self.profileButton.isSelected = !self.profileButton.isSelected
+         profileButton.rx.tap.subscribe(onNext: { [weak self] in
+             guard let self = self else { return }
+             self.profileButton.isSelected = !self.profileButton.isSelected
 
-            if self.profileButton.isSelected {
-                self.showEditSheet(in: self.view, type: .profile)
-                self.editSheetView.delegate = self
-            } else {
-                self.hideEditSheet()
-            }
-        }).disposed(by: disposeBag)
+             if self.profileButton.isSelected {
+                 self.showEditSheet(in: self.view, type: .profile)
+                 self.editSheetView.delegate = self
+             } else {
+                 self.hideEditSheet()
+             }
+         }).disposed(by: disposeBag)
 
-        Utility.PreferenceManager.$userInfo
-            .debug("$userInfo")
-            .filter { $0 != nil }
-            .subscribe(onNext: { [weak self] model in
-                guard let self = self, let model = model else {
-                    return
-                }
-                self.profileLabel.text = AES256.decrypt(encoded: model.name).correctionNickName
-                self.profileImageView.kf.setImage(
-                    with: URL(string: WMImageAPI.fetchProfile(name: model.profile, version: model.version).toString),
-                    placeholder: nil,
-                    options: [.transition(.fade(0.2))]
-                )
-            }).disposed(by: disposeBag)
-    }
+         Utility.PreferenceManager.$userInfo
+             .debug("$userInfo")
+             .filter { $0 != nil }
+             .subscribe(onNext: { [weak self] model in
+                 guard let self = self, let model = model else {
+                     return
+                 }
+                 self.profileLabel.text = AES256.decrypt(encoded: model.name).correctionNickName
+                 self.profileImageView.kf.setImage(
+                     with: URL(string: WMImageAPI.fetchProfile(name: model.profile, version: model.version).toString),
+                     placeholder: nil,
+                     options: [.transition(.fade(0.2))]
+                 )
+             }).disposed(by: disposeBag)
+     }
 
-    func bindEditButtonVisable() {
-        guard let vc1 = self.viewControllers[0] as? MyPlayListViewController else {
-            return
-        }
-        guard let vc2 = self.viewControllers[1] as? FavoriteViewController else {
-            return
-        }
+     func bindEditButtonVisable() {
+         guard let vc1 = self.viewControllers[0] as? MyPlayListViewController else {
+             return
+         }
+         guard let vc2 = self.viewControllers[1] as? FavoriteViewController else {
+             return
+         }
 
-        vc1.output.dataSource
-            .skip(1)
-            .filter { [weak self] _ in
-                guard let self = self else { return false }
-                return (self.currentIndex ?? 0) == 0
-            }
-            .map { ($0.first?.items ?? []).isEmpty }
-            .bind(to: editButton.rx.isHidden)
-            .disposed(by: disposeBag)
+         vc1.output.dataSource
+             .skip(1)
+             .filter { [weak self] _ in
+                 guard let self = self else { return false }
+                 return (self.currentIndex ?? 0) == 0
+             }
+             .map { ($0.first?.items ?? []).isEmpty }
+             .bind(to: editButton.rx.isHidden)
+             .disposed(by: disposeBag)
 
-        vc2.output.dataSource
-            .skip(1)
-            .filter { [weak self] _ in
-                guard let self = self else { return false }
-                return (self.currentIndex ?? 0) == 1
-            }
-            .map { ($0.first?.items ?? []).isEmpty }
-            .bind(to: editButton.rx.isHidden)
-            .disposed(by: disposeBag)
-    }
+         vc2.output.dataSource
+             .skip(1)
+             .filter { [weak self] _ in
+                 guard let self = self else { return false }
+                 return (self.currentIndex ?? 0) == 1
+             }
+             .map { ($0.first?.items ?? []).isEmpty }
+             .bind(to: editButton.rx.isHidden)
+             .disposed(by: disposeBag)
+     }
 
-    private func configureUI() {
-        profileImageView.layer.cornerRadius = 20
-        profileLabel.font = DesignSystemFontFamily.Pretendard.medium.font(size: 16)
+     private func configureUI() {
+         profileImageView.layer.cornerRadius = 20
+         profileLabel.font = DesignSystemFontFamily.Pretendard.medium.font(size: 16)
 
-        logoutButton.setImage(DesignSystemAsset.Storage.logout.image, for: .normal)
-        requestButton.setImage(DesignSystemAsset.Storage.request.image, for: .normal)
+         logoutButton.setImage(DesignSystemAsset.Storage.logout.image, for: .normal)
+         requestButton.setImage(DesignSystemAsset.Storage.request.image, for: .normal)
 
-        editButton.layer.cornerRadius = 4
-        editButton.layer.borderWidth = 1
-        editButton.backgroundColor = .clear
-        editButton.isHidden = true
+         editButton.layer.cornerRadius = 4
+         editButton.layer.borderWidth = 1
+         editButton.backgroundColor = .clear
+         editButton.isHidden = true
 
-        myPlayListFakeView.isHidden = true
-        favoriteFakeView.isHidden = true
+         myPlayListFakeView.isHidden = true
+         favoriteFakeView.isHidden = true
 
-        // 탭바 설정
-        self.dataSource = self
-        let bar = TMBar.ButtonBar()
+         // 탭바 설정
+         self.dataSource = self
+         let bar = TMBar.ButtonBar()
 
-        // 배경색
-        bar.backgroundView.style = .flat(color: .clear)
+         // 배경색
+         bar.backgroundView.style = .flat(color: .clear)
 
-        // 간격 설정
-        bar.layout.contentInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
-        bar.layout.contentMode = .intrinsic
-        bar.layout.transitionStyle = .progressive
+         // 간격 설정
+         bar.layout.contentInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+         bar.layout.contentMode = .intrinsic
+         bar.layout.transitionStyle = .progressive
 
-        // 버튼 글씨 커스텀
-        bar.buttons.customize { button in
-            button.tintColor = DesignSystemAsset.GrayColor.gray400.color
-            button.selectedTintColor = DesignSystemAsset.GrayColor.gray900.color
-            button.font = DesignSystemFontFamily.Pretendard.medium.font(size: 16)
-            button.selectedFont = DesignSystemFontFamily.Pretendard.bold.font(size: 16)
-        }
+         // 버튼 글씨 커스텀
+         bar.buttons.customize { button in
+             button.tintColor = DesignSystemAsset.GrayColor.gray400.color
+             button.selectedTintColor = DesignSystemAsset.GrayColor.gray900.color
+             button.font = DesignSystemFontFamily.Pretendard.medium.font(size: 16)
+             button.selectedFont = DesignSystemFontFamily.Pretendard.bold.font(size: 16)
+         }
 
-        // indicator
-        bar.indicator.weight = .custom(value: 3)
-        bar.indicator.tintColor = DesignSystemAsset.PrimaryColor.point.color
-        bar.indicator.overscrollBehavior = .compress
-        addBar(bar, dataSource: self, at: .custom(view: tabBarView, layout: nil))
-        bar.layer.addBorder(
-            [.bottom],
-            color: DesignSystemAsset.GrayColor.gray300.color.withAlphaComponent(0.4),
-            height: 1
-        )
-    }
-}
-*/
-
+         // indicator
+         bar.indicator.weight = .custom(value: 3)
+         bar.indicator.tintColor = DesignSystemAsset.PrimaryColor.point.color
+         bar.indicator.overscrollBehavior = .compress
+         addBar(bar, dataSource: self, at: .custom(view: tabBarView, layout: nil))
+         bar.layer.addBorder(
+             [.bottom],
+             color: DesignSystemAsset.GrayColor.gray300.color.withAlphaComponent(0.4),
+             height: 1
+         )
+     }
+ }
+ */
 
 extension StorageViewController: PageboyViewControllerDataSource, TMBarDataSource {
     public func numberOfViewControllers(in pageboyViewController: Pageboy.PageboyViewController) -> Int {
@@ -329,11 +310,11 @@ extension StorageViewController {
             favorite.scrollToTop()
         }
     }
-    
+
     public func equalHandleTapped() {
-           let viewControllersCount: Int = self.navigationController?.viewControllers.count ?? 0
-           if viewControllersCount > 1 {
-               self.navigationController?.popToRootViewController(animated: true)
-           }
+        let viewControllersCount: Int = self.navigationController?.viewControllers.count ?? 0
+        if viewControllersCount > 1 {
+            self.navigationController?.popToRootViewController(animated: true)
+        }
     }
 }
