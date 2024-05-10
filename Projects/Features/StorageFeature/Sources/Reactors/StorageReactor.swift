ba21e850d2e1cd8e996f0678aa04d7922568ae3f
Hamp <yongbeomkwak@gmail.com>
2024-05-10 15:57:24 +0900

    :recycle: :: 편집 이벤트 및 로그인 팝업 연결

--- Projects/Features/StorageFeature/Sources/Reactors/StorageReactor.swift
+++ Projects/Features/StorageFeature/Sources/Reactors/StorageReactor.swift
@@ -1,21 +1,90 @@
 import Foundation
 import ReactorKit
+import Utility
 
 final class StorageReactor: Reactor {
-    enum Action {}
 
-    enum Mutation {}
+    enum Action {
+        case switchTab(Int)
+        case tabDidEditButton
+        case saveButtonTap
+        case showLoginAlert
+    }
 
-    struct State {}
+    enum Mutation {
+        
+        case switchTabIndex(Int)
+        case switchEditingState(Bool)
+        case showLoginAlert
+        
+    }
+
+    struct State {
+        var isEditing: Bool
+        var tabIndex: Int
+        @Pulse var isShowLoginAlert: Void
+    }
 
     let initialState: State
 
     init() {
-        initialState = State (
+        initialState = State(
+            isEditing: false,
+            tabIndex: 0,
+            isShowLoginAlert: ()
         )
     }
 
-    func mutate(action: Action) -> Observable<Mutation> {}
+    func mutate(action: Action) -> Observable<Mutation> {
+        
+        switch action {
+            
+        case let .switchTab(index):
+            return switchTabIndex(index)
+        case let .tabDidEditButton:
+            return switchEditingState(true)
+        case .showLoginAlert:
+            return showLoginAlert()
+        case .saveButtonTap:
+            return switchEditingState(false)
+        }
+        
+        
+    }
 
-    func reduce(state: State, mutation: Mutation) -> State {}
+    func reduce(state: State, mutation: Mutation) -> State {
+        
+        var newState = state
+        
+        switch mutation {
+            
+        case let .switchTabIndex(index):
+            newState.tabIndex = index
+        case let .switchEditingState(flag):
+            newState.isEditing = flag
+        case .showLoginAlert:
+            newState.isShowLoginAlert = ()
+        }
+        
+        return newState
+        
+    }
+    
+}
+
+extension StorageReactor {
+    
+    func switchTabIndex(_ index: Int) -> Observable<Mutation> {
+        return .just(.switchTabIndex(index))
+    }
+    
+    func switchEditingState(_ flag: Bool) -> Observable<Mutation> {
+        
+        return .just(.switchEditingState(flag))
+    }
+    
+    func showLoginAlert() -> Observable<Mutation> {
+        return .just(.showLoginAlert)
+    }
+    
 }
