197e6fd6b4a5ab913505df0976efc1d8bacfc4c3
Hamp <yongbeomkwak@gmail.com>
2024-05-10 16:39:54 +0900

    :lipstick: :: 로그인 워닝 뷰

--- Projects/Features/StorageFeature/Sources/ViewControllers/StorageViewController.swift
+++ Projects/Features/StorageFeature/Sources/ViewControllers/StorageViewController.swift
@@ -282,34 +282,6 @@ extension StorageViewController {
 //             }).disposed(by: disposeBag)
     }
 
-    func bindEditButtonVisable() {
-        guard let vc1 = self.viewControllers[0] as? MyPlayListViewController else {
-            return
-        }
-        guard let vc2 = self.viewControllers[1] as? FavoriteViewController else {
-            return
-        }
-
-        vc1.output.dataSource
-            .skip(1)
-            .filter { [weak self] _ in
-                guard let self = self else { return false }
-                return (self.currentIndex ?? 0) == 0
-            }
-            .map { ($0.first?.items ?? []).isEmpty }
-            .bind(to: editButton.rx.isHidden)
-            .disposed(by: disposeBag)
-
-        vc2.output.dataSource
-            .skip(1)
-            .filter { [weak self] _ in
-                guard let self = self else { return false }
-                return (self.currentIndex ?? 0) == 1
-            }
-            .map { ($0.first?.items ?? []).isEmpty }
-            .bind(to: editButton.rx.isHidden)
-            .disposed(by: disposeBag)
-    }
 
     private func configureUI() {
         editButton.layer.cornerRadius = 4
