3d3412a30decdfce765dd9cbab468b835de8b653
Hamp <yongbeomkwak@gmail.com>
2024-05-10 15:57:35 +0900

    :bricks: :: 포맷팅

--- Projects/Features/StorageFeature/Sources/ViewControllers/StorageViewController.swift
+++ Projects/Features/StorageFeature/Sources/ViewControllers/StorageViewController.swift
@@ -13,14 +13,12 @@ import Utility
 
 final class StorageViewController: TabmanViewController, ViewControllerFromStoryBoard, EqualHandleTappedType,
     StoryboardView {
-    
     private enum Color {
         static let gray = DesignSystemAsset.GrayColor.gray400.color
 
         static let point = DesignSystemAsset.PrimaryColor.point.color
     }
-    
-    
+
     private enum ButtonAttributed {
         static let edit = NSMutableAttributedString(
             string: "편집",
@@ -38,7 +36,7 @@ final class StorageViewController: TabmanViewController, ViewControllerFromStory
             ]
         )
     }
-    
+
     typealias Reactor = StorageReactor
 
     var disposeBag: DisposeBag = DisposeBag()
@@ -127,62 +125,54 @@ final class StorageViewController: TabmanViewController, ViewControllerFromStory
 
 extension StorageViewController {
     func bindAction(reactor: Reactor) {
-        
         editButton.rx
             .tap
             .withUnretained(self)
-            .withLatestFrom(Utility.PreferenceManager.$userInfo){($0.0,$1)}
-            .bind { (owner,userInfo) in
-                
+            .withLatestFrom(Utility.PreferenceManager.$userInfo) { ($0.0, $1) }
+            .bind { owner, userInfo in
+
                 guard let userInfo = userInfo else {
                     reactor.action.onNext(.showLoginAlert) // 로그인 화면 팝업
                     return
                 }
-            
+
                 reactor.action.onNext(.tabDidEditButton)
             }
             .disposed(by: disposeBag)
-        
-        
+
         saveButton.rx
             .tap
-            .map { Reactor.Action.saveButtonTap}
+            .map { Reactor.Action.saveButtonTap }
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
-        
     }
 
     func bindState(reactor: Reactor) {
-        
         let sharedState = reactor.state.share(replay: 1)
-        
+
         sharedState.map(\.isEditing)
             .withUnretained(self)
-            .bind { owner,flag in
+            .bind { owner, flag in
 
                 owner.isScrollEnabled = !flag //  편집 시 , 옆 탭으로 swipe를 막기 위함
                 owner.editButton.isHidden = flag
                 owner.saveButton.isHidden = !flag
-                
-                 if flag {
-                     owner.myPlayListFakeView.isHidden = owner.currentIndex == 0
-                     owner.favoriteFakeView.isHidden = owner.currentIndex == 1
-                 } else {
-                     owner.myPlayListFakeView.isHidden = true
-                     owner.favoriteFakeView.isHidden = true
-                 }
-                
-                
+
+                if flag {
+                    owner.myPlayListFakeView.isHidden = owner.currentIndex == 0
+                    owner.favoriteFakeView.isHidden = owner.currentIndex == 1
+                } else {
+                    owner.myPlayListFakeView.isHidden = true
+                    owner.favoriteFakeView.isHidden = true
+                }
             }
             .disposed(by: disposeBag)
-        
+
         reactor.pulse(\.$isShowLoginAlert)
             .skip(1)
             .withUnretained(self)
-            .bind { (owner,_) in
-            
-             
-                
+            .bind { owner, _ in
+
                 guard let vc = owner.textPopUpFactory.makeView(
                     text: "로그인이 필요한 서비스입니다.\n로그인 하시겠습니까?",
                     cancelButtonIsHidden: false,
@@ -193,28 +183,19 @@ extension StorageViewController {
                         let loginVC = owner.signInFactory.makeView()
                         owner.present(loginVC, animated: true)
                     },
-                    cancelCompletion: {
-                        
-                    }
+                    cancelCompletion: {}
                 ) as? TextPopupViewController else {
                     return
                 }
-                
+
                 owner.showPanModal(content: vc)
-                
-                
-                
             }
             .disposed(by: disposeBag)
-        
-        
     }
 }
 
-
- extension StorageViewController {
-     
-     private func bindRx() {
+extension StorageViewController {
+    private func bindRx() {
 //         output.state.subscribe { [weak self] state in
 //             guard let self = self else {
 //                 return
@@ -232,7 +213,7 @@ extension StorageViewController {
 //                 .cgColor : DesignSystemAsset.GrayColor.gray300.color.cgColor
 //             self.editButton.setAttributedTitle(attr, for: .normal)
 //             self.isScrollEnabled = !state.isEditing //  편집 시 , 옆 탭으로 swipe를 막기 위함
-//           
+//
 //
 //             if state.isEditing {
 //                 self.myPlayListFakeView.isHidden = self.currentIndex == 0
@@ -299,90 +280,88 @@ extension StorageViewController {
 //                     options: [.transition(.fade(0.2))]
 //                 )
 //             }).disposed(by: disposeBag)
-     }
+    }
 
-     func bindEditButtonVisable() {
-         guard let vc1 = self.viewControllers[0] as? MyPlayListViewController else {
-             return
-         }
-         guard let vc2 = self.viewControllers[1] as? FavoriteViewController else {
-             return
-         }
+    func bindEditButtonVisable() {
+        guard let vc1 = self.viewControllers[0] as? MyPlayListViewController else {
+            return
+        }
+        guard let vc2 = self.viewControllers[1] as? FavoriteViewController else {
+            return
+        }
 
-         vc1.output.dataSource
-             .skip(1)
-             .filter { [weak self] _ in
-                 guard let self = self else { return false }
-                 return (self.currentIndex ?? 0) == 0
-             }
-             .map { ($0.first?.items ?? []).isEmpty }
-             .bind(to: editButton.rx.isHidden)
-             .disposed(by: disposeBag)
+        vc1.output.dataSource
+            .skip(1)
+            .filter { [weak self] _ in
+                guard let self = self else { return false }
+                return (self.currentIndex ?? 0) == 0
+            }
+            .map { ($0.first?.items ?? []).isEmpty }
+            .bind(to: editButton.rx.isHidden)
+            .disposed(by: disposeBag)
 
-         vc2.output.dataSource
-             .skip(1)
-             .filter { [weak self] _ in
-                 guard let self = self else { return false }
-                 return (self.currentIndex ?? 0) == 1
-             }
-             .map { ($0.first?.items ?? []).isEmpty }
-             .bind(to: editButton.rx.isHidden)
-             .disposed(by: disposeBag)
-     }
- 
-     private func configureUI() {
+        vc2.output.dataSource
+            .skip(1)
+            .filter { [weak self] _ in
+                guard let self = self else { return false }
+                return (self.currentIndex ?? 0) == 1
+            }
+            .map { ($0.first?.items ?? []).isEmpty }
+            .bind(to: editButton.rx.isHidden)
+            .disposed(by: disposeBag)
+    }
 
-         editButton.layer.cornerRadius = 4
-         editButton.layer.borderWidth = 1
-         editButton.backgroundColor = .clear
-         
-         editButton.layer.borderColor = DesignSystemAsset.GrayColor.gray300.color.cgColor
-         
-         editButton.setAttributedTitle(ButtonAttributed.edit, for: .normal)
-         
-         saveButton.layer.cornerRadius = 4
-         saveButton.layer.borderWidth = 1
-         saveButton.backgroundColor = .clear
-         saveButton.layer.borderColor = Color.point.cgColor
-         saveButton.setAttributedTitle(ButtonAttributed.save, for: .normal)
-         saveButton.isHidden = true
+    private func configureUI() {
+        editButton.layer.cornerRadius = 4
+        editButton.layer.borderWidth = 1
+        editButton.backgroundColor = .clear
 
-         myPlayListFakeView.isHidden = true
-         favoriteFakeView.isHidden = true
+        editButton.layer.borderColor = DesignSystemAsset.GrayColor.gray300.color.cgColor
 
-         // 탭바 설정
-         self.dataSource = self
-         let bar = TMBar.ButtonBar()
+        editButton.setAttributedTitle(ButtonAttributed.edit, for: .normal)
 
-         // 배경색
-         bar.backgroundView.style = .flat(color: .clear)
+        saveButton.layer.cornerRadius = 4
+        saveButton.layer.borderWidth = 1
+        saveButton.backgroundColor = .clear
+        saveButton.layer.borderColor = Color.point.cgColor
+        saveButton.setAttributedTitle(ButtonAttributed.save, for: .normal)
+        saveButton.isHidden = true
 
-         // 간격 설정
-         bar.layout.contentInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
-         bar.layout.contentMode = .intrinsic
-         bar.layout.transitionStyle = .progressive
+        myPlayListFakeView.isHidden = true
+        favoriteFakeView.isHidden = true
 
-         // 버튼 글씨 커스텀
-         bar.buttons.customize { button in
-             button.tintColor = DesignSystemAsset.GrayColor.gray400.color
-             button.selectedTintColor = DesignSystemAsset.GrayColor.gray900.color
-             button.font = DesignSystemFontFamily.Pretendard.medium.font(size: 16)
-             button.selectedFont = DesignSystemFontFamily.Pretendard.bold.font(size: 16)
-         }
+        // 탭바 설정
+        self.dataSource = self
+        let bar = TMBar.ButtonBar()
 
-         // indicator
-         bar.indicator.weight = .custom(value: 3)
-         bar.indicator.tintColor = DesignSystemAsset.PrimaryColor.point.color
-         bar.indicator.overscrollBehavior = .compress
-         addBar(bar, dataSource: self, at: .custom(view: tabBarView, layout: nil))
-         bar.layer.addBorder(
-             [.bottom],
-             color: DesignSystemAsset.GrayColor.gray300.color.withAlphaComponent(0.4),
-             height: 1
-         )
-     }
- }
+        // 배경색
+        bar.backgroundView.style = .flat(color: .clear)
 
+        // 간격 설정
+        bar.layout.contentInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        bar.layout.contentMode = .intrinsic
+        bar.layout.transitionStyle = .progressive
+
+        // 버튼 글씨 커스텀
+        bar.buttons.customize { button in
+            button.tintColor = DesignSystemAsset.GrayColor.gray400.color
+            button.selectedTintColor = DesignSystemAsset.GrayColor.gray900.color
+            button.font = DesignSystemFontFamily.Pretendard.medium.font(size: 16)
+            button.selectedFont = DesignSystemFontFamily.Pretendard.bold.font(size: 16)
+        }
+
+        // indicator
+        bar.indicator.weight = .custom(value: 3)
+        bar.indicator.tintColor = DesignSystemAsset.PrimaryColor.point.color
+        bar.indicator.overscrollBehavior = .compress
+        addBar(bar, dataSource: self, at: .custom(view: tabBarView, layout: nil))
+        bar.layer.addBorder(
+            [.bottom],
+            color: DesignSystemAsset.GrayColor.gray300.color.withAlphaComponent(0.4),
+            height: 1
+        )
+    }
+}
 
 extension StorageViewController: PageboyViewControllerDataSource, TMBarDataSource {
     public func numberOfViewControllers(in pageboyViewController: Pageboy.PageboyViewController) -> Int {
