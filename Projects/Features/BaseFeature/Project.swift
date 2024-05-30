ec4e1580381aaf69e41305f3f3a2e2129105742b
youn9k <ace_lephant@naver.com>
2024-05-30 16:22:15 +0900

    🔨 :: BaseFeature -> ErrorModule 의존성 추가

--- Projects/Features/BaseFeature/Project.swift
+++ Projects/Features/BaseFeature/Project.swift
@@ -2,6 +2,7 @@ import DependencyPlugin
 import ProjectDescription
 import ProjectDescriptionHelpers
 
+#warning("에러 모듈위치 변경 필요")
 let project = Project.module(
     name: ModulePaths.Feature.BaseFeature.rawValue,
     targets: [
@@ -17,6 +18,7 @@ let project = Project.module(
                     .domain(target: .AuthDomain, type: .interface),
                     .domain(target: .PlayListDomain, type: .interface),
                     .domain(target: .UserDomain, type: .interface),
+                    .module(target: .ErrorModule),
                     .Project.Module.FeatureThirdPartyLib,
                     .Project.UserInterfaces.DesignSystem,
                     .Project.Module.Utility
