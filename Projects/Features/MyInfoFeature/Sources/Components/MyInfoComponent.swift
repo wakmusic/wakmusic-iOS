e00073f72f7c4e1596e44e0fb002a6dcc0e317c6
youn9k <ace_lephant@naver.com>
2024-05-30 09:27:24 +0900

    💄 :: [#550] 이미지 리소스 추가 및 LoginWarningView

--- Projects/Features/MyInfoFeature/Sources/Components/MyInfoComponent.swift
+++ Projects/Features/MyInfoFeature/Sources/Components/MyInfoComponent.swift
@@ -6,6 +6,7 @@ public protocol MyInfoDependency: Dependency {}
 
 public final class MyInfoComponent: Component<MyInfoDependency>, MyInfoFactory {
     public func makeView() -> UIViewController {
-        return MyInfoViewController()
+        let reactor = MyInfoReactor()
+        return MyInfoViewController(reactor: reactor)
     }
 }
--- Projects/Features/MyInfoFeature/Sources/ViewControllers/MyInfoViewController.swift
+++ Projects/Features/MyInfoFeature/Sources/ViewControllers/MyInfoViewController.swift
@@ -1,15 +1,38 @@
 import BaseFeature
+import DesignSystem
+import Foundation
+import LogManager
+import MyInfoFeatureInterface
+import RxSwift
+import SnapKit
+import Then
 import UIKit
+import Utility
 
-/// TODO:
-class MyInfoViewController: UIViewController, EqualHandleTappedType {
-    override func viewDidLoad() {
-        super.viewDidLoad()
+final class MyInfoViewController: BaseReactorViewController<MyInfoReactor> {
+    let loginWarningView = LoginWarningView(text: "로그인을 해주세요.")
 
-        self.view.backgroundColor = .red
+    override func configureNavigation() {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
-}
 
-extension MyInfoViewController {
-    func equalHandleTapped() {}
+    override func addView() {
+        self.view.addSubview(loginWarningView)
+    }
+
+    override func setLayout() {
+        self.view.backgroundColor = DesignSystemAsset.BlueGrayColor.blueGray100.color
+
+        loginWarningView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+
+    override func bindState(reactor: MyInfoReactor) {}
+
+    override func bindAction(reactor: MyInfoReactor) {
+        loginWarningView.rx.loginButtonDidTap.subscribe { _ in
+            print("로그인 버튼 눌림")
+        }.disposed(by: disposeBag)
+    }
 }
