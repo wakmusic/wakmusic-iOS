9eb07a859e2e769e086d6470e4a7083a433e7d59
KTH <pikagreen@nate.com>
2024-06-01 14:58:36 +0900

    🔧 :: LyricHighlighting 코드 파일 생성

---
+++ Projects/Features/LyricHighlightingFeature/Sources/ViewControllers/LyricHighlightingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LyricHighlightingViewController.swift
+//  LyricHighlightingFeature
+//
+//  Created by KTH on 6/1/24.
+//  Copyright © 2024 yongbeomkwak. All rights reserved.
+//
+
+import UIKit
+
+class LyricHighlightingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Projects/Features/LyricHighlightingFeature/Sources/Views/LyricHighlightingCell.swift
@@ -0,0 +1,16 @@
+//
+//  LyricHighlightingCell.swift
+//  LyricHighlightingFeature
+//
+//  Created by KTH on 6/1/24.
+//  Copyright © 2024 yongbeomkwak. All rights reserved.
+//
+
+import UIKit
+
+class LyricHighlightingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
