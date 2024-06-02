1102f5e06873e306d2a2dbc6ec8b86b097b114ee
Hamp <yongbeomkwak@gmail.com>
2024-06-02 14:28:30 +0900

    :bricks: :: 임시 커밋

---
+++ Projects/Domains/BaseDomain/Interface/Entity/ImageModel.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+
+public struct ImageModel {
+    private let type: Int // 1: default, 2: custom
+    private var imageName: String?
+    private var file: String?
+    
+    public init(type: Int, imageName: String? = nil, file: String? = nil) {
+        self.type = type
+        self.imageName = imageName
+        self.file = file
+    }
+    
+    
+    var toMultipartFormData :  {
+        
+        switch self.type {
+        case 1: break
+            
+        default: break
+        }
+        
+    }
+}
--- Projects/Features/BaseFeature/Sources/Protocols/ImageUploadable.swift
+++ Projects/Features/BaseFeature/Sources/Protocols/ImageUploadable.swift
@@ -1,9 +1,24 @@
 import BaseDomainInterface
 import Foundation
+import UIKit
+
+extension String {
+    
+    func convertImageModel(_ image: String, isCustom: Bool = false) -> ImageModel {
+        return isCustom ?  ImageModel(type: 2, file: image) :  ImageModel(type: 1, imageName: image)
+    }
+    
+}
+
 
 public protocol ImageUploadable {
     /// 1 ~ 11
-    func uploadDefaultImage(_ image: String) -> ImageType
+    func uploadImage(_ image: String) -> ImageModel
 
-    func uploadCustomImage(_ image: String) -> ImageType
+}
+
+public extension ImageUploadable  where Self: UIViewController {
+    
+    #warning("Sheet 구현 시 연결")
+    
 }
