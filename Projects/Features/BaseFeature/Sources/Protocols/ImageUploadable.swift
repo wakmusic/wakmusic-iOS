246bd4e08dfd448d71187e2bfbae9561aa4cdeb6
Hamp <yongbeomkwak@gmail.com>
2024-06-02 14:28:45 +0900

    :bricks: :: 포맷팅

--- Projects/Features/BaseFeature/Sources/Protocols/ImageUploadable.swift
+++ Projects/Features/BaseFeature/Sources/Protocols/ImageUploadable.swift
@@ -3,22 +3,16 @@ import Foundation
 import UIKit
 
 extension String {
-    
     func convertImageModel(_ image: String, isCustom: Bool = false) -> ImageModel {
-        return isCustom ?  ImageModel(type: 2, file: image) :  ImageModel(type: 1, imageName: image)
+        return isCustom ? ImageModel(type: 2, file: image) : ImageModel(type: 1, imageName: image)
     }
-    
 }
-
 
 public protocol ImageUploadable {
     /// 1 ~ 11
     func uploadImage(_ image: String) -> ImageModel
-
 }
 
-public extension ImageUploadable  where Self: UIViewController {
-    
+public extension ImageUploadable where Self: UIViewController {
     #warning("Sheet 구현 시 연결")
-    
 }
