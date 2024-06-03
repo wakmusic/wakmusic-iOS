850e4e0045e2765c404b5a7c1f2e1e7d53eef974
Hamp <yongbeomkwak@gmail.com>
2024-06-03 21:00:39 +0900

    :zap: :: 왁뮤 추천 리액터 생성

---
+++ Projects/Features/SearchFeature/Sources/Reactors/WakmusicRecommendReactor.swift
@@ -0,0 +1,75 @@
+import Foundation
+import ReactorKit
+import LogManager
+import PlayListDomainInterface
+import RxSwift
+
+final class WakmusicRecommendReactor: Reactor {
+    
+    private var disposeBag: DisposeBag = DisposeBag()
+    private var fetchRecommendPlayListUseCase: any FetchRecommendPlayListUseCase
+    
+    var initialState: State
+    
+    
+    enum Action {
+        case viewDidLoad
+    }
+    
+    enum Mutation {
+        case updateDataSource([RecommendPlayListEntity])
+    }
+    
+    struct State {
+        var dataSource: [RecommendPlayListEntity]
+    }
+
+    func mutate(action: Action) -> Observable<Mutation> {
+        
+        switch action {
+            case .viewDidLoad:
+                return updateDataSource()
+        }
+        
+    }
+    
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+
+        var newState = state
+        
+        switch mutation {
+            case let .updateDataSource(dataSource):
+                newState.dataSource = dataSource
+        }
+        
+        return newState
+    }
+    
+    
+    init(fetchRecommendPlayListUseCase: any FetchRecommendPlayListUseCase) {
+        
+        LogManager.printDebug("✅ \(Self.self)")
+        self.fetchRecommendPlayListUseCase = fetchRecommendPlayListUseCase
+        self.initialState = State(
+            dataSource: []
+        )
+    }
+    
+    deinit {
+        LogManager.printDebug("❌ \(Self.self)")
+    }
+    
+}
+
+extension WakmusicRecommendReactor {
+    
+    func updateDataSource() -> Observable<Mutation> {
+        
+        return fetchRecommendPlayListUseCase
+                .execute()
+                .asObservable()
+                .map{Mutation.updateDataSource($0)}
+    
+    }
+}
