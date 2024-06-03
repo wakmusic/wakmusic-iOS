8284e60d844fd13c2770eae4c01c4ab18a974976
Hamp <yongbeomkwak@gmail.com>
2024-06-04 00:20:21 +0900

    :art: ::  이전 PR 리뷰 반영

--- Projects/Domains/PlayListDomain/Interface/Entity/AddSongEntity.swift
+++ Projects/Domains/PlayListDomain/Interface/Entity/AddSongEntity.swift
@@ -1,22 +1,15 @@
-//
-//  AddSongEntity.swift
-//  DomainModule
-//
-//  Created by yongbeomkwak on 2023/03/14.
-//  Copyright © 2023 yongbeomkwak. All rights reserved.
-//
-
 import Foundation
 
 public struct AddSongEntity: Equatable {
     public init(
-        added_songs_length: Int,
+        addedSongCount: Int,
         duplicated: Bool
     ) {
-        self.added_songs_length = added_songs_length
+        self.addedSongCount = addedSongCount
         self.duplicated = duplicated
     }
 
-    public let added_songs_length: Int
+    public let addedSongCount: Int
     public let duplicated: Bool
+    
 }
