b554fba3c9b4c088c3454c56fd4d914804a2a069
Hamp <yongbeomkwak@gmail.com>
2024-06-03 20:05:23 +0900

    :zap: :: 왁뮤 추천 플리 뷰컨

---
+++ Projects/Features/SearchFeature/Sources/ViewControllers/WakmusicRecommendViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WakmusicRecommendViewController.swift
+//  SearchFeature
+//
+//  Created by yongbeomkwak on 6/3/24.
+//  Copyright © 2024 yongbeomkwak. All rights reserved.
+//
+
+import UIKit
+
+class WakmusicRecommendViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
