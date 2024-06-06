77560258e61c0920f478a11e111d50dc8e48b611
pikagreen@nate.com <pikagreen@nate.com>
2024-06-06 23:26:49 +0900

    💡 :: [#491] 주석 추가

--- Projects/Features/LyricHighlightingFeature/Interface/LyricHighlightingRequiredModel.swift
+++ Projects/Features/LyricHighlightingFeature/Interface/LyricHighlightingRequiredModel.swift
@@ -6,11 +6,12 @@ public struct LyricHighlightingRequiredModel {
     public let artist: String
     public let highlightingItems: [String]
 
+    //  가사 하이라이팅 화면에 주입해줘야할 필수 아이템
     public init(
-        songID: String,
-        title: String,
-        artist: String,
-        highlightingItems: [String] = []
+        songID: String, // 노래 고유 ID
+        title: String,  // 노래 제목
+        artist: String, // 아티스트 이름
+        highlightingItems: [String] = [] // 데코레이팅 화면으로 넘어갈 때만 넣으면됩니다.
     ) {
         self.songID = songID
         self.title = title
