f8c4431d8762e9085c795a9ecb206e336c817d58
Hamp <yongbeomkwak@gmail.com>
2024-06-17 18:14:18 +0900

    :zap: CI개선

--- Scripts/NeedleRunScript.sh
+++ Scripts/NeedleRunScript.sh
@@ -5,9 +5,16 @@ fi
 export PATH
 
 if which needle > /dev/null; then
+
 	ROOT_PATH="Projects/App"
-	touch "${ROOT_PATH}/Sources/Application/NeedleGenerated.swift"
-	needle generate ${ROOT_PATH}/Sources/Application/NeedleGenerated.swift ../
+
+	if [ $# -eq 0 ]; then #로컬 
+		needle generate Sources/Application/NeedleGenerated.swift ../
+	else #ci
+		touch "${ROOT_PATH}/Sources/Application/NeedleGenerated.swift"
+		needle generate ${ROOT_PATH}/Sources/Application/NeedleGenerated.swift ../
+	fi
+
 else
 	echo "warning: Needle not installed, plz run 'brew install needle'"
 fi
