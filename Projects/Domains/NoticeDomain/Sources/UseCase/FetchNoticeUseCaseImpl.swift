ab589acf662be073eec9663c39cd9d8ac6a513d8
youn9k <ace_lephant@naver.com>
2024-06-17 12:45:07 +0900

    🎨 :: [#568] Notice Domain useCase stub 추가

--- Projects/Domains/NoticeDomain/Sources/UseCase/FetchNoticeUseCaseImpl.swift
+++ Projects/Domains/NoticeDomain/Sources/UseCase/FetchNoticeUseCaseImpl.swift
@@ -1,11 +1,3 @@
-//
-//  FetchNoticeUseCaseImpl.swift
-//  DataModule
-//
-//  Created by KTH on 2023/04/08.
-//  Copyright © 2023 yongbeomkwak. All rights reserved.
-//
-
 import Foundation
 import NoticeDomainInterface
 import RxSwift
