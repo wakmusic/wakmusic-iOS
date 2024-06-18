4575069958af616a4d2c701827375b0568aa0a6e
youn9k <ace_lephant@naver.com>
2024-06-18 10:21:19 +0900

    🐛 ::  사용되지 않는 매개변수 제거

--- Projects/Features/BaseFeature/Testing/TextPopUpComponentStub.swift
+++ Projects/Features/BaseFeature/Testing/TextPopUpComponentStub.swift
@@ -6,7 +6,6 @@ public final class TextPopUpComponentStub: TextPopUpFactory {
     public func makeView(
         text: String?,
         cancelButtonIsHidden: Bool,
-        allowsDragAndTapToDismiss: Bool?,
         confirmButtonText: String?,
         cancelButtonText: String?,
         completion: (() -> Void)?,
@@ -15,7 +14,6 @@ public final class TextPopUpComponentStub: TextPopUpFactory {
         return TextPopupViewController.viewController(
             text: text ?? "",
             cancelButtonIsHidden: cancelButtonIsHidden,
-            allowsDragAndTapToDismiss: allowsDragAndTapToDismiss ?? true,
             confirmButtonText: confirmButtonText ?? "확인",
             cancelButtonText: cancelButtonText ?? "취소",
             completion: completion,
