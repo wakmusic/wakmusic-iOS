c13e0ea5775723d27410881b4d5be2187aa76d4a
Hamp <yongbeomkwak@gmail.com>
2024-06-22 20:37:59 +0900

    :zap: :: dataSource moved 관련 함수 override

--- Projects/Features/PlaylistFeature/Sources/DataSource/MyplaylistDetailDataSource.swift
+++ Projects/Features/PlaylistFeature/Sources/DataSource/MyplaylistDetailDataSource.swift
@@ -10,9 +10,34 @@ final class MyplaylistDetailDataSource: UITableViewDiffableDataSource<Int, SongE
         true
     }
 
-    override func tableView(
-        _ tableView: UITableView,
-        moveRowAt sourceIndexPath: IndexPath,
-        to destinationIndexPath: IndexPath
-    ) {}
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+               guard let sourceIdentifier = itemIdentifier(for: sourceIndexPath) else { return }
+               guard sourceIndexPath != destinationIndexPath else { return }
+               let destinationIdentifier = itemIdentifier(for: destinationIndexPath)
+                print(sourceIndexPath, destinationIndexPath)
+               var snapshot = self.snapshot()
+               
+               // 같은 섹션으로 이동
+               if let destinationIdentifier = destinationIdentifier {
+                   if let sourceIndex = snapshot.indexOfItem(sourceIdentifier),
+                      let destinationIndex = snapshot.indexOfItem(destinationIdentifier) {
+                       let isAfter = destinationIndex > sourceIndex &&
+                           snapshot.sectionIdentifier(containingItem: sourceIdentifier) ==
+                           snapshot.sectionIdentifier(containingItem: destinationIdentifier)
+                       snapshot.deleteItems([sourceIdentifier])
+                       if isAfter {
+                           snapshot.insertItems([sourceIdentifier], afterItem: destinationIdentifier)
+                       } else {
+                           snapshot.insertItems([sourceIdentifier], beforeItem: destinationIdentifier)
+                       }
+                   }
+               } else {
+
+                   // 다른 섹션으로 이동
+                   let destinationSectionIdentifier = snapshot.sectionIdentifiers[destinationIndexPath.section]
+                   snapshot.deleteItems([sourceIdentifier])
+                   snapshot.appendItems([sourceIdentifier], toSection: destinationSectionIdentifier)
+               }
+               apply(snapshot, animatingDifferences: false)
+           }
 }
