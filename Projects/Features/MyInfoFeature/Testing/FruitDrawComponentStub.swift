39dd8f36c47359e3e11cfbd1bfe8070738879fec
pikagreen@nate.com <pikagreen@nate.com>
2024-06-26 19:16:45 +0900

    🔥 :: [#664] FruitDrawComponentStub

--- Projects/Features/MyInfoFeature/Testing/FruitDrawComponentStub.swift
+++
@@ -1,9 +0,0 @@
-@testable import FruitDrawFeature
-import FruitDrawFeatureInterface
-import UIKit
-
-public final class FruitDrawComponentStub: FruitDrawFactory {
-    public func makeView() -> UIViewController {
-        return FruitDrawViewController(viewModel: .init())
-    }
-}
--- Projects/Features/MyInfoFeature/Testing/MyInfoComponentStub.swift
+++ Projects/Features/MyInfoFeature/Testing/MyInfoComponentStub.swift
@@ -8,16 +8,6 @@ import UIKit
 
 public final class MyInfoComponentStub: MyInfoFactory {
     public func makeView() -> UIViewController {
-        return MyInfoViewController.viewController(
-            reactor: MyInfoReactor(),
-            textPopUpFactory: TextPopUpComponentStub(),
-            signInFactory: SignInComponentStub(),
-            faqFactory: FaqComponentStub(),
-            noticeFactory: NoticeComponentStub(),
-            questionFactory: QuestionComponentStub(),
-            teamInfoFactory: TeamInfoComponentStub(),
-            settingFactory: SettingComponentStub(),
-            fruitDrawFactory: FruitDrawComponentStub()
-        )
+        return UIViewController()
     }
 }
