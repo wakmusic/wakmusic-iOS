72c7cdf5fa9b60aea620b31f0da50b91a52fb917
youn9k <ace_lephant@naver.com>
2024-06-26 19:17:14 +0900

    🐛 ::  방해되는 코드 삭제

--- Projects/Features/MyInfoFeature/Testing/MyInfoComponentStub.swift
+++ Projects/Features/MyInfoFeature/Testing/MyInfoComponentStub.swift
@@ -8,15 +8,6 @@ import UIKit
 
 public final class MyInfoComponentStub: MyInfoFactory {
     public func makeView() -> UIViewController {
-        return MyInfoViewController.viewController(
-            reactor: MyInfoReactor(),
-            textPopUpFactory: TextPopUpComponentStub(),
-            signInFactory: SignInComponentStub(),
-            faqFactory: FaqComponentStub(),
-            noticeFactory: NoticeComponentStub(),
-            questionFactory: QuestionComponentStub(),
-            teamInfoFactory: TeamInfoComponentStub(),
-            settingFactory: SettingComponentStub()
-        )
+        return UIViewController()
     }
 }
