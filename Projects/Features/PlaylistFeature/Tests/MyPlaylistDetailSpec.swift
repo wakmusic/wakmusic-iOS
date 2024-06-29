c2b8f0258eb854761c7d79377546b9f93436633d
Hamp <yongbeomkwak@gmail.com>
2024-06-29 13:49:53 +0900

    :zap: :: 테스트 코드 제거

--- Projects/Features/PlaylistFeature/Tests/MyPlaylistDetailSpec.swift
+++ Projects/Features/PlaylistFeature/Tests/MyPlaylistDetailSpec.swift
@@ -6,23 +6,6 @@ import XCTest
 
 final class MyPlaylistDetailSpec: QuickSpec {
     override class func spec() {
-        var reactor: MyPlaylistDetailReactor = MyPlaylistDetailReactor(key: "TT")
 
-        describe("MyPlaylistDetailReactorTest") {
-//            context("viewDidLoad") {
-//                reactor.action.onNext(.viewDidLoad)
-//
-//                let state = reactor.currentState
-//
-//                // expect(state.dataSource.songs.count).to(equal(9))
-//
-//                print(state.dataSource.songs.map{$0.title})
-//                reactor.action.onNext(.itemDidMoved(0, 8))
-//
-//                print(state.dataSource.songs.map{$0.title})
-//            }
-//
-//
-        }
     }
 }
