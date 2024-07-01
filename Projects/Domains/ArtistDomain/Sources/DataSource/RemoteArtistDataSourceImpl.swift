41363c8b2fc9bea77adfb864aef0e5d8999f8c38
pikagreen@nate.com <pikagreen@nate.com>
2024-07-02 01:44:19 +0900

    🎨 :: 코드 Formatting 적용

--- Projects/Domains/ArtistDomain/Sources/DataSource/RemoteArtistDataSourceImpl.swift
+++ Projects/Domains/ArtistDomain/Sources/DataSource/RemoteArtistDataSourceImpl.swift
@@ -21,7 +21,7 @@ public final class RemoteArtistDataSourceImpl: BaseRemoteDataSource<ArtistAPI>, 
             .map(ArtistSubscriptionStatusResponseDTO.self)
             .map { $0.toDomain() }
     }
-    
+
     public func subscriptionArtist(id: String, on: Bool) -> Completable {
         request(.subscriptionArtist(id: id, on: on))
             .asCompletable()
