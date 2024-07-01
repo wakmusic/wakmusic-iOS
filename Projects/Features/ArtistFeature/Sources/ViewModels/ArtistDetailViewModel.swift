a6e77bce5c44d50eb24d76b139b8ce32c2941f4b
pikagreen@nate.com <pikagreen@nate.com>
2024-07-02 01:55:15 +0900

    🎨 :: 코드 Formatting 적용

--- Projects/Features/ArtistFeature/Sources/ViewModels/ArtistDetailViewModel.swift
+++ Projects/Features/ArtistFeature/Sources/ViewModels/ArtistDetailViewModel.swift
@@ -1,15 +1,15 @@
-import Foundation
-import BaseFeature
 import ArtistDomainInterface
-import RxSwift
+import BaseFeature
+import Foundation
 import RxRelay
+import RxSwift
 
 public final class ArtistDetailViewModel: ViewModelType {
     private let model: ArtistListEntity
     private let fetchArtistSubscriptionStatusUseCase: FetchArtistSubscriptionStatusUseCase
     private let subscriptionArtistUseCase: SubscriptionArtistUseCase
     private let disposeBag = DisposeBag()
-    
+
     public init(
         model: ArtistListEntity,
         fetchArtistSubscriptionStatusUseCase: any FetchArtistSubscriptionStatusUseCase,
@@ -19,15 +19,15 @@ public final class ArtistDetailViewModel: ViewModelType {
         self.fetchArtistSubscriptionStatusUseCase = fetchArtistSubscriptionStatusUseCase
         self.subscriptionArtistUseCase = subscriptionArtistUseCase
     }
-    
+
     public struct Input {
         let fetchArtistSubscriptionStatus: PublishSubject<Void> = PublishSubject()
     }
-    
+
     public struct Output {
         let isSubscription: BehaviorRelay<Bool> = BehaviorRelay(value: false)
     }
-    
+
     public func transform(from input: Input) -> Output {
         let output = Output()
         return output
