2cc6f7dcf64e3ee5f23b033c9f511577859ce144
Hamp <yongbeomkwak@gmail.com>
2024-07-06 20:44:21 +0900

    :zap: :: 간격  조절

--- Projects/Features/PlaylistFeature/Sources/ViewControllers/DefaultPlaylistImageViewController.swift
+++ Projects/Features/PlaylistFeature/Sources/ViewControllers/DefaultPlaylistImageViewController.swift
@@ -199,19 +199,20 @@ extension DefaultPlaylistImageViewController {
 
             let item = NSCollectionLayoutItem(layoutSize: itemSize)
 
+            
             let groupSize = NSCollectionLayoutSize(
                 widthDimension: .fractionalWidth(1.0),
-                heightDimension: .absolute(76.0)
+                heightDimension: .fractionalWidth(0.25)
             )
 
             var group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 4)
+        
             group.interItemSpacing = .fixed(10)
 
             let section = NSCollectionLayoutSection(group: group)
-            section.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 20, bottom: .zero, trailing: 20)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 20, bottom: 20, trailing: 20)
 
-            section.interGroupSpacing = 10.0
-
+            
             return section
         }
         return layout
