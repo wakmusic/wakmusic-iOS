b54cae31487618c859b0eb7047f406645e84c363
baegteun <baegteun@gmail.com>
2024-07-18 00:02:28 +0900

    :sparkles: [#793] Multiple ID Youtube Play 지원

--- Projects/Modules/Utility/Sources/Player/WakmusicYoutubePlayer.swift
+++ Projects/Modules/Utility/Sources/Player/WakmusicYoutubePlayer.swift
@@ -3,30 +3,63 @@ import UIKit
 
 public struct WakmusicYoutubePlayer: WakmusicPlayer {
     private let youtubeURLGenerator: any YoutubeURLGeneratable
-    private let id: String
+    private let ids: [String]
 
     public init(
         id: String,
         youtubeURLGenerator: any YoutubeURLGeneratable = YoutubeURLGenerator()
     ) {
-        self.id = id
+        self.ids = [id]
+        self.youtubeURLGenerator = youtubeURLGenerator
+    }
+
+    public init(
+        ids: [String],
+        youtubeURLGenerator: any YoutubeURLGeneratable = YoutubeURLGenerator()
+    ) {
+        self.ids = ids
         self.youtubeURLGenerator = youtubeURLGenerator
     }
 
     public func play() {
-        playYoutube(id: id)
+        playYoutube(ids: ids)
     }
 }
 
 private extension WakmusicYoutubePlayer {
-    func playYoutube(id: String) {
-        if let youtubeAppURL = URL(string: youtubeURLGenerator.generateYoutubeVideoAppURL(id: id)),
-           UIApplication.shared.canOpenURL(youtubeAppURL) {
-            UIApplication.shared.open(youtubeAppURL)
-        } else if
-            let youtubeWebURL = URL(string: youtubeURLGenerator.generateYoutubeVideoWebURL(id: id)),
-            UIApplication.shared.canOpenURL(youtubeWebURL) {
-            UIApplication.shared.open(youtubeWebURL)
+    func playYoutube(ids: [String]) {
+        if let appURL = urlForYoutubeApp(ids: ids) {
+            UIApplication.shared.open(appURL)
+        } else if let webURL = urlForYoutubeWeb(ids: ids) {
+            UIApplication.shared.open(webURL)
         }
+    }
+
+    func urlForYoutubeApp(ids: [String]) -> URL? {
+        if ids.count == 1,
+           let id = ids.first,
+           let youtubeAppURL = URL(string: youtubeURLGenerator.generateYoutubeVideoAppURL(id: id)),
+           UIApplication.shared.canOpenURL(youtubeAppURL) {
+            return youtubeAppURL
+        } else if
+            let youtubeAppURL = URL(string: youtubeURLGenerator.generateYoutubeVideoAppURL(ids: ids)),
+            UIApplication.shared.canOpenURL(youtubeAppURL) {
+            return youtubeAppURL
+        }
+        return nil
+    }
+
+    func urlForYoutubeWeb(ids: [String]) -> URL? {
+        if ids.count == 1,
+           let id = ids.first,
+           let youtubeWebURL = URL(string: youtubeURLGenerator.generateYoutubeVideoWebURL(id: id)),
+           UIApplication.shared.canOpenURL(youtubeWebURL) {
+            return youtubeWebURL
+        } else if
+            let youtubeWebURL = URL(string: youtubeURLGenerator.generateYoutubeVideoWebURL(ids: ids)),
+            UIApplication.shared.canOpenURL(youtubeWebURL) {
+            return youtubeWebURL
+        }
+        return nil
     }
 }
