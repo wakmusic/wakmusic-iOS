c283698df1997061674c46755f919c5d5e4ae5ab
KTH <pikagreen@nate.com>
2024-07-19 02:38:41 +0900

    ✨ :: [#779] TeamFeature 모듈 생성 및 디펜던시 연결

---
+++ Projects/Features/TeamFeature/Project.swift
@@ -0,0 +1,24 @@
+import DependencyPlugin
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(
+    name: ModulePaths.Feature.TeamFeature.rawValue,
+    targets: [
+        .interface(module: .feature(.TeamFeature)),
+        .implements(
+            module: .feature(.TeamFeature),
+            dependencies: [
+                .feature(target: .TeamFeature, type: .interface),
+                .feature(target: .BaseFeature),
+                .domain(target: .TeamDomain, type: .interface)
+            ]
+        ),
+        .demo(
+            module: .feature(.TeamFeature),
+            dependencies: [
+                .feature(target: .TeamFeature)
+            ]
+        )
+    ]
+)
