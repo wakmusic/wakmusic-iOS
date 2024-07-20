bdd22680d2f4d0a8566bfb7c0eaecf3ad90c72c7
KTH <pikagreen@nate.com>
2024-07-20 21:06:32 +0900

    ✨ :: [#831] LocalizationStrings 적용

--- Projects/Features/ArtistFeature/Sources/ViewControllers/ArtistMusicContentViewController.swift
+++ Projects/Features/ArtistFeature/Sources/ViewControllers/ArtistMusicContentViewController.swift
@@ -9,6 +9,7 @@ import RxSwift
 import SongsDomainInterface
 import UIKit
 import Utility
+import Localization
 
 public class ArtistMusicContentViewController: BaseViewController, ViewControllerFromStoryBoard, SongCartViewType {
     @IBOutlet weak var tableView: UITableView!
@@ -162,7 +163,7 @@ extension ArtistMusicContentViewController: SongCartViewDelegate {
 
         case .addSong:
             guard songs.count <= limit else {
-                output.showToast.onNext("곡 수가 \(limit)개를 초과했습니다.\n노래담기는 최대 \(limit)곡까지 가능합니다.")
+                output.showToast.onNext(LocalizationStrings.overFlowContainWarning)
                 return
             }
             let songIds: [String] = songs.map { $0.id }
@@ -174,7 +175,7 @@ extension ArtistMusicContentViewController: SongCartViewDelegate {
 
         case .addPlayList:
             guard songs.count <= limit else {
-                output.showToast.onNext("곡 수가 \(limit)개를 초과했습니다.\n재생목록추가는 최대 \(limit)곡까지 가능합니다.")
+                output.showToast.onNext(LocalizationStrings.overFlowAddPlaylistWarning)
                 return
             }
             PlayState.shared.appendSongsToPlaylist(songs)
@@ -182,7 +183,7 @@ extension ArtistMusicContentViewController: SongCartViewDelegate {
 
         case .play:
             guard songs.count <= limit else {
-                output.showToast.onNext("곡 수가 \(limit)개를 초과했습니다.\n재생은 최대 \(limit)곡까지 가능합니다.")
+                output.showToast.onNext(LocalizationStrings.overFlowPlayWarning)
                 return
             }
             PlayState.shared.loadAndAppendSongsToPlaylist(songs)
