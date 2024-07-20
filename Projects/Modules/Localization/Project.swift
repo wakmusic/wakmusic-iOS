d82f987b9631ec17f2c14f8749af724e77a9b265
Hamp <yongbeomkwak@gmail.com>
2024-07-20 19:48:37 +0900

    :zap:: 인터페이스 제거

--- Projects/Modules/Localization/Project.swift
+++ Projects/Modules/Localization/Project.swift
@@ -5,15 +5,12 @@ import ProjectDescriptionHelpers
 let project = Project.module(
     name: ModulePaths.Module.Localization.rawValue,
     targets: [
-        .interface(module: .module(.Localization)),
         .implements(
             module: .module(.Localization),
             product: .staticFramework,
             spec: .init(
                 resources: ["Resources/**"],
-                dependencies: [
-                    .module(target: .Localization, type: .interface)
-                ]
+                dependencies: []
             )
         ),
     ]
