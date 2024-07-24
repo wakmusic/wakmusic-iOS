831fcc41d476fdd1dcced1e9e9b69f0317b80686
yongbeomkwak <yongbeomkwak@gmail.com>
2024-07-24 23:30:12 +0900

    🎨 :: 코드 Formatting 적용

--- Projects/Features/PlaylistFeature/Sources/ViewControllers/MyPlaylistDetailViewController.swift
+++ Projects/Features/PlaylistFeature/Sources/ViewControllers/MyPlaylistDetailViewController.swift
@@ -593,11 +593,9 @@ extension MyPlaylistDetailViewController: PHPickerViewControllerDelegate {
                         DispatchQueue.main.async {
                             guard let image = image as? UIImage,
                                   let imageRawData = image.jpegData(compressionQuality: 1.0) else { return } // 80% 압축
-                            
-                            
-                           
+
                             let sizeMB: Double = Double(imageRawData.count).megabytes
-                            
+
                             let compressImageData = image.jpegData(compressionQuality: 0.8)
 
                             self.reactor?.action.onNext(.changeThumnail(sizeMB > 10 ? compressImageData : imageRawData))
