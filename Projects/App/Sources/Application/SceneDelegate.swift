9a1e0f8cd7bc05caa64a845060c7f71e3b9bdebe
pikagreen@nate.com <pikagreen@nate.com>
2024-07-27 00:40:24 +0900

    ⚡️ :: [#863] 딥링크 구현

--- Projects/App/Sources/Application/SceneDelegate.swift
+++ Projects/App/Sources/Application/SceneDelegate.swift
@@ -4,9 +4,12 @@ import NaverThirdPartyLogin
 import RootFeature
 import UIKit
 import Utility
+import LogManager
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    private var root: AppComponent?
+
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
@@ -15,9 +18,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: scene)
         registerProviderFactories()
-        let root = AppComponent()
-        self.window?.rootViewController = root.makeRootView().wrapNavigationController
+        self.root = AppComponent()
+        self.window?.rootViewController = root?.makeRootView().wrapNavigationController
         self.window?.makeKeyAndVisible()
+
+        /// Not Running Handling App Entry
+        handleAppEntry(with: connectionOptions)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {}
@@ -35,7 +41,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let url = URLContexts.first?.url else { return }
 
         let scheme: String = url.scheme ?? ""
-        DEBUG_LOG("[openURLContexts] scheme: \(scheme), URL: \(url.absoluteString)")
+        LogManager.printDebug("[openURLContexts] scheme: \(scheme), URL: \(url.absoluteString)")
 
         switch scheme {
         case GOOGLE_URL_SCHEME(): // 구글
@@ -43,6 +49,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         case NAVER_URL_SCHEME(): // 네이버
             NaverThirdPartyLoginConnection.getSharedInstance().receiveAccessToken(url)
+
+        case WM_URI_SCHEME():
+            handleDeeplink(url: url)
 
         default: return
         }
@@ -54,6 +63,46 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
               let webpageURL = userActivity.webpageURL else {
             return
         }
-        DEBUG_LOG(webpageURL.absoluteString)
+        LogManager.printDebug("URL: \(webpageURL.absoluteString)")
+
+        guard webpageURL.host == WM_UNIVERSALLINK_DOMAIN() else {
+            return
+        }
+        handleUniversalLink(url: webpageURL)
+    }
+}
+
+private extension SceneDelegate {
+    func handleAppEntry(with connectionOptions: UIScene.ConnectionOptions) {
+        if let url = connectionOptions.urlContexts.first?.url {
+            handleDeeplink(url: url)
+        } else if connectionOptions.userActivities.first?.activityType == NSUserActivityTypeBrowsingWeb,
+                  let webpageURL = userActivity?.webpageURL {
+            handleUniversalLink(url: webpageURL)
+        } else if let _ = connectionOptions.notificationResponse?.notification {
+        #warning("TO-DO:: Push Notification")
+            // notification.request.content.userInfo
+        }
+    }
+
+    func handleDeeplink(url: URL) {
+        let page: String = url.host ?? ""
+        let pathComponents: [String] = url.pathComponents.filter { $0 != "/" }
+        var params: [String: Any] = url.params()
+        params["page"] = page
+        LogManager.printDebug("host: \(page), pathComponents: \(pathComponents), params: \(params)")
+        root?.appEntryState.moveScene(params: params)
+    }
+
+    func handleUniversalLink(url: URL) {
+        let host: String = url.host ?? ""
+        let pathComponents: [String] = url.pathComponents.filter { $0 != "/" }
+        let page: String = pathComponents.first ?? ""
+        let key: String = pathComponents.last ?? ""
+        var params: [String: Any] = url.params()
+        params["page"] = page
+        params["key"] = key
+        LogManager.printDebug("host: \(host), pathComponents: \(pathComponents), params: \(params)")
+        root?.appEntryState.moveScene(params: params)
     }
 }
