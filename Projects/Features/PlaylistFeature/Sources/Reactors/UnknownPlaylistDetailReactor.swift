6762d2e87155a494530e9de6654e1aef126140b2
Hamp <yongbeomkwak@gmail.com>
2024-07-27 12:07:39 +0900

    :zap: :: 로그인 전 처리

--- Projects/Features/PlaylistFeature/Sources/Reactors/UnknownPlaylistDetailReactor.swift
+++ Projects/Features/PlaylistFeature/Sources/Reactors/UnknownPlaylistDetailReactor.swift
@@ -217,18 +217,24 @@ private extension UnknownPlaylistDetailReactor {
         let currentState = currentState
 
         let prev = currentState.isSubscribing
+        
+        if PreferenceManager.userInfo == nil {
+            return .just(.showToast("구독 기능은 로그인 후 이용할 수 있습니다."))
+        }
+        else {
+            return subscribePlaylistUseCase.execute(key: key, isSubscribing: prev)
+                .andThen(
+                    .concat([
+                        .just(Mutation.updateSubscribeState(!prev)),
+                        .just(Mutation.showToast(prev ? "리스트 구독을 취소 했습니다." : "리스트 구독을 했습니다."))
+                    ])
+                )
+                .catch { error in
+                    let wmError = error.asWMError
+                    return .just(.showToast(wmError.errorDescription!))
+                }
+        }
 
-        return subscribePlaylistUseCase.execute(key: key, isSubscribing: prev)
-            .andThen(
-                .concat([
-                    .just(Mutation.updateSubscribeState(!prev)),
-                    .just(Mutation.showToast(prev ? "리스트 구독을 취소 했습니다." : "리스트 구독을 했습니다."))
-                ])
-            )
-            .catch { error in
-                let wmError = error.asWMError
-                return .just(.showToast(wmError.errorDescription!))
-            }
     }
 
     func updateSubscribeState(_ flag: Bool) -> Observable<Mutation> {
