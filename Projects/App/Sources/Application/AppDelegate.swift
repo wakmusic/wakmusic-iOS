3585dd825ca610269a06a9ebc4ec60d46670493b
pikagreen@nate.com <pikagreen@nate.com>
2024-08-04 20:32:01 +0900

    🎨 :: 코드 Formatting 적용

--- Projects/App/Sources/Application/AppDelegate.swift
+++ Projects/App/Sources/Application/AppDelegate.swift
@@ -13,7 +13,6 @@ import Utility
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
-
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
