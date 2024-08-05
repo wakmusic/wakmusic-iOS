8e04b37b09f481d5b02ca00d3a46933092a383b6
youn9k <ace_lephant@naver.com>
2024-08-05 09:52:18 +0900

    🎨 :: 코드 Formatting 적용

--- Projects/Modules/Utility/Sources/Extensions/Extension+Double.swift
+++ Projects/Modules/Utility/Sources/Extensions/Extension+Double.swift
@@ -17,19 +17,19 @@ public extension Double {
     var gigabytes: Double {
         return megabytes / 1024
     }
-    
+
     var correctLeading: CGFloat {
         return self * APP_WIDTH() / 375.0
     }
-    
+
     var correctTrailing: CGFloat {
         return -self * APP_WIDTH() / 375.0
     }
-    
+
     var correctTop: CGFloat {
         return APP_HEIGHT() * (self / 812.0)
     }
-    
+
     var correctBottom: CGFloat {
         return -APP_HEIGHT() * (self / 812.0)
     }
