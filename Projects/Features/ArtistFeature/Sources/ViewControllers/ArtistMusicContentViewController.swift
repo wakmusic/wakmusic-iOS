617e9da6c5eef22d7a3b03aad1d0e69b3cce0fea
KTH <pikagreen@nate.com>
2024-08-12 02:06:29 +0900

    ✨ :: [#1069] present 로직 변경 2t

--- Projects/Features/ArtistFeature/Sources/ViewControllers/ArtistMusicContentViewController.swift
+++ Projects/Features/ArtistFeature/Sources/ViewControllers/ArtistMusicContentViewController.swift
@@ -212,13 +212,7 @@ private extension ArtistMusicContentViewController {
 
 extension ArtistMusicContentViewController: ArtistMusicCellDelegate {
     func tappedThumbnail(id: String) {
-        if let presentingViewController = self.presentingViewController {
-            presentingViewController.dismiss(animated: true) { [songDetailPresenter] () in
-                songDetailPresenter?.present(id: id)
-            }
-        } else {
-            songDetailPresenter.present(id: id)
-        }
+        songDetailPresenter.present(id: id)
     }
 }
 
