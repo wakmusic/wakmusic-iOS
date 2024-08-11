da0b41f6b082876cef5b38b81f99d6a6465d5510
baegteun <baegteun@gmail.com>
2024-08-11 17:57:22 +0900

    :sparkles: :: [#1064] 아티스트 화면 진입을 위한 Credit API 변경사항 반영

--- Projects/Domains/SongsDomain/Sources/ResponseDTO/SongCreditsResponseDTO.swift
+++ Projects/Domains/SongsDomain/Sources/ResponseDTO/SongCreditsResponseDTO.swift
@@ -11,14 +11,32 @@ import SongsDomainInterface
 
 public struct SongCreditsResponseDTO: Decodable {
     let type: String
-    let names: [String]
+    let names: [Credit]
+
+    public struct Credit: Decodable {
+        let name: String
+        let isArtist: Bool
+        let artistID: String?
+
+        enum CodingKeys: String, CodingKey {
+            case name
+            case isArtist = "isArtist"
+            case artistID = "artistId"
+        }
+    }
 }
 
 public extension SongCreditsResponseDTO {
     func toDomain() -> SongCreditsEntity {
         return SongCreditsEntity(
             type: type,
-            names: names
+            names: names.map {
+                SongCreditsEntity.Credit(
+                    name: $0.name,
+                    isArtist: $0.isArtist,
+                    artistID: $0.artistID
+                )
+            }
         )
     }
 }
