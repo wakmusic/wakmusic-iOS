2c6efabd4c5f328b3141b08ce78c5a0558d22c23
KTH <pikagreen@nate.com>
2024-08-12 15:52:51 +0900

    🐛 :: [#1074] 카테고리가 전체만 노출되는 오류 수정

--- Projects/Domains/FaqDomain/Sources/DataSource/RemoteFaqDataSourceImpl.swift
+++ Projects/Domains/FaqDomain/Sources/DataSource/RemoteFaqDataSourceImpl.swift
@@ -6,8 +6,8 @@ import RxSwift
 public final class RemoteFaqDataSourceImpl: BaseRemoteDataSource<FaqAPI>, RemoteFaqDataSource {
     public func fetchCategories() -> Single<FaqCategoryEntity> {
         return request(.fetchFaqCategories)
-            .map(FaqCategoryResponseDTO.self)
-            .map { $0.toDomain() }
+            .map([String].self)
+            .map { FaqCategoryEntity(categories: $0) }
     }
 
     public func fetchQna() -> Single<[FaqEntity]> {
