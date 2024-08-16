c16b99afcc4e06f7b9e7066bbef011112e9f225b
youn9k <ace_lephant@naver.com>
2024-08-17 00:46:14 +0900

    📈  :: [#1149] 로그 노션 정리 및 enum case 추가

--- Projects/Features/MyInfoFeature/Sources/Analytics/SettingAnalyticsLog.swift
+++ Projects/Features/MyInfoFeature/Sources/Analytics/SettingAnalyticsLog.swift
@@ -1,3 +1,24 @@
 import LogManager
 
-enum SettingAnalyticsLog: AnalyticsLogType {}
+enum SettingAnalyticsLog: AnalyticsLogType {
+    /*
+     설정 화면 보여짐 - common
+     
+     알림 설정 버튼을 누름
+     서비스 이용약관 버튼을 누름
+     개인정보 처리방침 버튼을 누름
+     오픈소스 라이선스 버튼을 누름
+     캐시 데이터 지우기 버튼을 누름
+     로그아웃 버튼을 누름
+     버전정보 버튼을 누름
+     회원탈퇴 버튼을 누름
+     */
+    
+    case clickNotificationButton
+    case clickServiceInfoButton
+    case clickOpensourceButton
+    case clickRemoveCacheButton
+    case clickLogoutButton
+    case clickVersionButton
+    case clickWithdrawButton
+}
