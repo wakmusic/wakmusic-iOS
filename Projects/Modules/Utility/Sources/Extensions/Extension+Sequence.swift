4f0f8bb73d3b15323505328e12222d26df8257e2
baegteun <baegteun@gmail.com>
2024-09-22 13:59:04 +0900

    🎨 :: 코드 Formatting 적용

--- Projects/Modules/Utility/Sources/Extensions/Extension+Sequence.swift
+++ Projects/Modules/Utility/Sources/Extensions/Extension+Sequence.swift
@@ -1,21 +1,21 @@
 import Foundation
 
-extension Sequence where Element: Hashable {
+public extension Sequence where Element: Hashable {
     @inlinable
-    public func uniqued() -> UniquedSequence<Self, Element> {
+    func uniqued() -> UniquedSequence<Self, Element> {
         UniquedSequence(base: self, projection: { $0 })
     }
 }
 
-extension Sequence {
+public extension Sequence {
     @inlinable
-    public func uniqued<Subject: Hashable>(
+    func uniqued<Subject: Hashable>(
         on projection: (Element) throws -> Subject
     ) rethrows -> [Element] {
         var seen: Set<Subject> = []
         var result: [Element] = []
         for element in self {
-            if seen.insert(try projection(element)).inserted {
+            if try seen.insert(projection(element)).inserted {
                 result.append(element)
             }
         }
@@ -23,9 +23,9 @@ extension Sequence {
     }
 }
 
-extension LazySequenceProtocol {
+public extension LazySequenceProtocol {
     @inlinable
-    public func uniqued<Subject: Hashable>(
+    func uniqued<Subject: Hashable>(
         on projection: @escaping (Element) -> Subject
     ) -> UniquedSequence<Self, Subject> {
         UniquedSequence(base: self, projection: projection)
