8ec8fa46b835478bb8c84e84f032927fcba702e5
Hamp <yongbeomkwak@gmail.com>
2024-10-26 16:22:50 +0900

    :sparkles: :: 기간에 맞는 앱 아이콘 변경

--- Projects/Features/RootFeature/Sources/ViewModels/SplashLogoType.swift
+++ Projects/Features/RootFeature/Sources/ViewModels/SplashLogoType.swift
@@ -1,15 +1,26 @@
-//
-//  SplashLogoType.swift
-//  RootFeature
-//
-//  Created by KTH on 10/22/24.
-//  Copyright © 2024 yongbeomkwak. All rights reserved.
-//
-
-import Foundation
+import UIKit
 
 enum SplashLogoType: String {
     case usual = "Splash_Logo_Main"
     case halloween = "Splash_Logo_Halloween"
     case xmas = "Splash_Logo_Xmas"
+    
+    var icon: String? {
+        switch self {
+        case .usual:
+            return nil
+        case .halloween:
+            return "HalloweenAppIcon"
+        case .xmas:
+            return "XmasAppIcon"
+        }
+    }
+}
+
+func changeAppIcon(_ type: SplashLogoType) {
+    guard UIApplication.shared.alternateIconName != type.icon else {
+        return
+    }
+    
+    UIApplication.shared.setAlternateIconName(type.icon)
 }
