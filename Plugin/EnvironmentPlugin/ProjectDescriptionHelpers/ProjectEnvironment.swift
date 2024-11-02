d002a6a47c3afc5036b1bb6fddf7a91e44b9e29f
Hamp <yongbeomkwak@gmail.com>
2024-11-02 14:35:03 +0900

    :sparkles: :: 버전 업 3.2.0

--- Plugin/EnvironmentPlugin/ProjectDescriptionHelpers/ProjectEnvironment.swift
+++ Plugin/EnvironmentPlugin/ProjectDescriptionHelpers/ProjectEnvironment.swift
@@ -19,7 +19,7 @@ public let env = ProjectEnvironment(
     deploymentTargets: .iOS("15.0"),
     destinations: [.iPhone],
     baseSetting: SettingsDictionary()
-        .marketingVersion("3.1.0")
+        .marketingVersion("3.2.0")
         .currentProjectVersion("0")
         .debugInformationFormat(DebugInformationFormat.dwarfWithDsym)
         .otherLinkerFlags(["-ObjC"])
