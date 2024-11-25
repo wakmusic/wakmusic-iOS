d44a0ca040449c136b8a053be7d2078dde2437f5
baegteun <baegteun@gmail.com>
2024-11-25 23:01:59 +0900

    :recycle: :: [#1314] Missing Sendable Conformance

--- Projects/Domains/AuthDomain/Testing/CheckIsExistAccessTokenUseCaseStub.swift
+++ Projects/Domains/AuthDomain/Testing/CheckIsExistAccessTokenUseCaseStub.swift
@@ -1,7 +1,7 @@
 import AuthDomainInterface
 import RxSwift
 
-public struct CheckIsExistAccessTokenUseCaseStub: CheckIsExistAccessTokenUseCase {
+public struct CheckIsExistAccessTokenUseCaseStub: CheckIsExistAccessTokenUseCase, @unchecked Sendable {
     public func execute() -> Single<Bool> {
         return .just(false)
     }
--- Projects/Domains/NoticeDomain/Testing/FetchNoticeCategoriesUseCaseStub.swift
+++ Projects/Domains/NoticeDomain/Testing/FetchNoticeCategoriesUseCaseStub.swift
@@ -2,7 +2,7 @@ import Foundation
 import NoticeDomainInterface
 import RxSwift
 
-public struct FetchNoticeCategoriesUseCaseStub: FetchNoticeCategoriesUseCase {
+public struct FetchNoticeCategoriesUseCaseStub: FetchNoticeCategoriesUseCase @unchecked Sendable {
     public func execute() -> Single<FetchNoticeCategoriesEntity> {
         return .just(FetchNoticeCategoriesEntity(categories: []))
     }
